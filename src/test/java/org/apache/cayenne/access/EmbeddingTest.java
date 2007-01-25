61f93c056e56a69f7952ce769d20b2f1699df229
Andrus Adamchik <aadamchik@apache.org>
2007-01-25 13:37:04 +0000

    CAY-736: Embeddable class support by Cayenne runtime (initial setup of the unit tests)
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk/framework/cayenne-jdk1.4-unpublished@499786 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/cayenne/access/EmbeddingTest.java
@@ -0,0 +1,48 @@
+/*****************************************************************
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ ****************************************************************/
+package org.apache.cayenne.access;
+
+import org.apache.cayenne.query.SelectQuery;
+import org.apache.cayenne.testdo.embeddable.EmbedEntity1;
+import org.apache.cayenne.unit.AccessStack;
+import org.apache.cayenne.unit.CayenneCase;
+import org.apache.cayenne.unit.CayenneResources;
+
+public class EmbeddingTest extends CayenneCase {
+
+    public static final String EMBEDDING_ACCESS_STACK = "EmbeddingStack";
+
+    protected AccessStack buildAccessStack() {
+        return CayenneResources.getResources().getAccessStack(EMBEDDING_ACCESS_STACK);
+    }
+
+    public void testSelect() throws Exception {
+        createTestData("testSelect");
+
+        SelectQuery query = new SelectQuery(EmbedEntity1.class);
+        query.addOrdering(EmbedEntity1.NAME_PROPERTY, true);
+
+        // TODO: andrus 1/25/2007 - this fails
+
+        // ObjectContext context = createDataContext();
+
+        // List results = context.performQuery(query);
+        // assertEquals(2, results.size());
+    }
+}
