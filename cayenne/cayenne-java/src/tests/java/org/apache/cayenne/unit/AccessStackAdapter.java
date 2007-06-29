6610e8fa805ef32b4e6b684e92ca8ce8af19dc1a
Andrus Adamchik <aadamchik@apache.org>
2007-06-29 13:02:57 +0000

    CAY-116 - temp fix for unittests
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/branches/STABLE-2.0@551879 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne/cayenne-java/src/tests/java/org/apache/cayenne/unit/AccessStackAdapter.java
+++ cayenne/cayenne-java/src/tests/java/org/apache/cayenne/unit/AccessStackAdapter.java
@@ -76,6 +76,11 @@ public class AccessStackAdapter {
      */
     public void willDropTables(Connection conn, DataMap map, Collection tablesToDrop)
             throws Exception {
+        dropConstraints(conn, map, tablesToDrop);
+    }
+
+    protected void dropConstraints(Connection conn, DataMap map, Collection tablesToDrop)
+            throws Exception {
 
         if (adapter.supportsFkConstraints()) {
             Map constraintsMap = getConstraints(conn, map, tablesToDrop);
