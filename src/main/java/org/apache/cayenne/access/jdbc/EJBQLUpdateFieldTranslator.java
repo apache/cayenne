d1f95f32068e9056c3c822ff99d40eee251275e9
Andrus Adamchik <aadamchik@apache.org>
2007-08-01 19:42:44 +0000

    CAY-837 EJBQL Update Statement Support (translator refactoring to reuse Condition translator or UpdateField which is semantically very close)
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk/framework/cayenne-jdk1.4-unpublished@561931 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/cayenne/access/jdbc/EJBQLUpdateFieldTranslator.java
@@ -0,0 +1,58 @@
+/*****************************************************************
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ ****************************************************************/
+package org.apache.cayenne.access.jdbc;
+
+import org.apache.cayenne.ejbql.EJBQLException;
+import org.apache.cayenne.ejbql.EJBQLExpression;
+
+/**
+ * @since 3.0
+ * @author Andrus Adamchik
+ */
+class EJBQLUpdateFieldTranslator extends EJBQLConditionTranslator {
+
+    public EJBQLUpdateFieldTranslator(EJBQLTranslationContext context) {
+        super(context);
+    }
+
+    public boolean visitUpdateField(EJBQLExpression expression, int finishedChildIndex) {
+
+        EJBQLPathTranslator pathTranslator = new EJBQLPathTranslator(context) {
+
+            protected void appendMultiColumnPath(EJBQLMultiColumnOperand operand) {
+                throw new EJBQLException(
+                        "Multi-column paths are not yet supported in UPDATEs");
+            }
+
+            public boolean visitUpdateField(
+                    EJBQLExpression expression,
+                    int finishedChildIndex) {
+                return visitPath(expression, finishedChildIndex);
+            }
+        };
+
+        expression.visit(pathTranslator);
+        return false;
+    }
+
+    public boolean visitUpdateValue(EJBQLExpression expression) {
+        context.append(" =");
+        return true;
+    }
+}
