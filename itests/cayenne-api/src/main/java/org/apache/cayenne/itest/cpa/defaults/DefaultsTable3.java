a85da13b71748001c8f766aae9492072a1800079
Andrus Adamchik <aadamchik@apache.org>
2007-08-08 14:30:15 +0000

    CAY-843 Remove arbitrary reverse relationship mapping limitations (integration tests)
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@563899 13f79535-47bb-0310-9956-ffa450edef68

---
+++ itests/cayenne-api/src/main/java/org/apache/cayenne/itest/cpa/defaults/DefaultsTable3.java
@@ -0,0 +1,10 @@
+package org.apache.cayenne.itest.cpa.defaults;
+
+import org.apache.cayenne.itest.cpa.defaults.auto._DefaultsTable3;
+
+public class DefaultsTable3 extends _DefaultsTable3 {
+
+}
+
+
+
---
+++ itests/cayenne-api/src/test/java/org/apache/cayenne/itest/cpa/conf/RuntimeLoaderDelegateDefaultsLoadingTest.java
@@ -0,0 +1,85 @@
+package org.apache.cayenne.itest.cpa.conf;
+
+import org.apache.cayenne.DataObjectUtils;
+import org.apache.cayenne.itest.cpa.CPAContextCase;
+import org.apache.cayenne.itest.cpa.defaults.DefaultsTable2;
+import org.apache.cayenne.itest.cpa.defaults.DefaultsTable3;
+import org.apache.cayenne.itest.cpa.defaults.DefaultsTable4;
+import org.apache.cayenne.map.DbEntity;
+import org.apache.cayenne.map.ObjEntity;
+import org.apache.cayenne.query.RefreshQuery;
+
+public class RuntimeLoaderDelegateDefaultsLoadingTest extends CPAContextCase {
+
+	public void testLoadedReverseDb() {
+
+		DbEntity table1 = context.getEntityResolver().getDbEntity(
+				"defaults_table1");
+		DbEntity table2 = context.getEntityResolver().getDbEntity(
+				"defaults_table2");
+		assertNotNull(table1.getAnyRelationship(table2));
+		assertTrue(table1.getAnyRelationship(table2).isRuntime());
+		assertFalse(table2.getAnyRelationship(table1).isRuntime());
+	}
+
+	public void testLoadedReverseObj() {
+
+		ObjEntity class1 = context.getEntityResolver().getObjEntity(
+				"DefaultsTable1");
+		ObjEntity class2 = context.getEntityResolver().getObjEntity(
+				"DefaultsTable2");
+		assertNotNull(class1.getAnyRelationship(class2));
+		assertTrue(class1.getAnyRelationship(class2).isRuntime());
+		assertFalse(class2.getAnyRelationship(class1).isRuntime());
+	}
+
+	public void testResolveRelationship() throws Exception {
+		getDbHelper().deleteAll("defaults_table2");
+		getDbHelper().deleteAll("defaults_table1");
+		getDbHelper().insert("defaults_table1", new String[] { "id", "name" },
+				new Object[] { 1, "X" });
+		getDbHelper().insert("defaults_table2",
+				new String[] { "id", "defaults_table1_id" },
+				new Object[] { 1, 1 });
+
+		DefaultsTable2 o = (DefaultsTable2) DataObjectUtils.objectForPK(
+				context, DefaultsTable2.class, 1);
+		assertNotNull(o.getToTable1());
+		assertEquals("X", o.getToTable1().getName());
+	}
+
+	public void testUpdateImplicitToMany() throws Exception {
+		getDbHelper().deleteAll("defaults_table4");
+		getDbHelper().deleteAll("defaults_table3");
+		getDbHelper().insert("defaults_table3", new String[] { "id", "name" },
+				new Object[] { 1, "X" });
+		getDbHelper().insert("defaults_table3", new String[] { "id", "name" },
+				new Object[] { 2, "Y" });
+		getDbHelper().insert("defaults_table4",
+				new String[] { "id", "defaults_table3_id" },
+				new Object[] { 1, 1 });
+
+		DefaultsTable4 o = (DefaultsTable4) DataObjectUtils.objectForPK(
+				context, DefaultsTable4.class, 1);
+		DefaultsTable3 o1 = (DefaultsTable3) DataObjectUtils.objectForPK(
+				context, DefaultsTable3.class, 1);
+		DefaultsTable3 o2 = (DefaultsTable3) DataObjectUtils.objectForPK(
+				context, DefaultsTable3.class, 2);
+
+		assertEquals(1, o1.getDefaultTable4s().size());
+		assertEquals(0, o2.getDefaultTable4s().size());
+
+		o2.addToDefaultTable4s(o);
+
+		assertEquals(1, o2.getDefaultTable4s().size());
+		context.commitChanges();
+		assertEquals(1, o2.getDefaultTable4s().size());
+
+		context.performQuery(new RefreshQuery());
+
+		// note that the old to-many is only refreshed after invalidation with
+		// RefreshQuery... should this be treated as a bug?
+		assertEquals(0, o1.getDefaultTable4s().size());
+		assertEquals(1, o2.getDefaultTable4s().size());
+	}
+}
