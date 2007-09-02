fb3f7e1943b1542b72c558a30dec70b6e41ab9f5
Andrus Adamchik <aadamchik@apache.org>
2007-09-02 15:35:23 +0000

    CAY-757 JPA enhancer should enhance undeclared classes
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@572002 13f79535-47bb-0310-9956-ffa450edef68

--- itests/jpa-chapter6/src/test/java/org/apache/cayenne/jpa/itest/ch6/_6_1_2_6_PersistentClassSourcesTest.java
+++ itests/jpa-chapter6/src/test/java/org/apache/cayenne/jpa/itest/ch6/_6_1_2_6_PersistentClassSourcesTest.java
@@ -1,16 +1,18 @@
 package org.apache.cayenne.jpa.itest.ch6;
 
 import org.apache.cayenne.itest.jpa.EntityManagerCase;
+import org.apache.cayenne.jpa.itest.ch6.entity.UndeclaredEntity1;
 
 public class _6_1_2_6_PersistentClassSourcesTest extends EntityManagerCase {
 
-	public void testLoadUndeclared() throws Exception {
+	public void testLoadImplicitFromUnitRoot() throws Exception {
+
 		getDbHelper().deleteAll("UndeclaredEntity1");
 
-//		UndeclaredEntity1 e = new UndeclaredEntity1();
-//		getEntityManager().persist(e);
-//		getEntityManager().getTransaction().commit();
-//
-//		assertEquals(1, getDbHelper().getRowCount("UndeclaredEntity1"));
+		UndeclaredEntity1 e = new UndeclaredEntity1();
+		getEntityManager().persist(e);
+		getEntityManager().getTransaction().commit();
+
+		assertEquals(1, getDbHelper().getRowCount("UndeclaredEntity1"));
 	}
 }
