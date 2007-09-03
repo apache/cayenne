e0ca6028585ad5c5cf22b335e1d3d761a23e7e8d
Andrus Adamchik <aadamchik@apache.org>
2007-09-03 08:06:57 +0000

    JPA itests: one-to-many defaults
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@572249 13f79535-47bb-0310-9956-ffa450edef68

--- itests/jpa-chapter2/src/test/resources/schema-hsqldb.sql
+++ itests/jpa-chapter2/src/test/resources/schema-hsqldb.sql
@@ -16,6 +16,8 @@ insert into AUTO_PK_SUPPORT (TABLE_NAME, NEXT_ID) VALUES ('PropertyDefaultsDates
 insert into AUTO_PK_SUPPORT (TABLE_NAME, NEXT_ID) VALUES ('PropertyDefaultsOtherEntity', 1);
 insert into AUTO_PK_SUPPORT (TABLE_NAME, NEXT_ID) VALUES ('BidiOneToOneOwner', 1);
 insert into AUTO_PK_SUPPORT (TABLE_NAME, NEXT_ID) VALUES ('BidiOneToOneOwned', 1);
+insert into AUTO_PK_SUPPORT (TABLE_NAME, NEXT_ID) VALUES ('BidiOneToManyOwner', 1);
+insert into AUTO_PK_SUPPORT (TABLE_NAME, NEXT_ID) VALUES ('BidiOneToManyOwned', 1);
 
 create table FieldPersistenceEntity (id int not null, property1 VARCHAR(100), primary key(id));
 create table PropertyPersistenceEntity (id int not null, property1 VARCHAR(100), property2 BOOLEAN, primary key(id));
@@ -39,3 +41,6 @@ create table BidiOneToOneOwned (id int not null, primary key(id));
 create table BidiOneToOneOwner (id int not null, owned_id int not null, primary key(id));
 alter table BidiOneToOneOwner add constraint cfk_owned_id FOREIGN KEY (owned_id) references BidiOneToOneOwned (id);
 alter table BidiOneToOneOwner add constraint cu_owned_id UNIQUE (owned_id);
+create table BidiOneToManyOwned (id int not null, primary key(id));
+create table BidiOneToManyOwner (id int not null, owned_id int not null, primary key(id));
+alter table BidiOneToManyOwner add constraint cfk_owned_id1 FOREIGN KEY (owned_id) references BidiOneToManyOwned (id);
