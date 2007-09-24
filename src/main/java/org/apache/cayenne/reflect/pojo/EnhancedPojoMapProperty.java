96dfdb514031fba44989f6a03058a26664bc06b2
Andrus Adamchik <aadamchik@apache.org>
2007-09-24 07:38:54 +0000

    CAY-848 Support for mapping to-many as Maps and Sets and Collections post commit to-many remapping
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk/framework/cayenne-jdk1.4-unpublished@578688 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/cayenne/reflect/pojo/EnhancedPojoMapProperty.java
+++ src/main/java/org/apache/cayenne/reflect/pojo/EnhancedPojoMapProperty.java
@@ -18,25 +18,71 @@
  ****************************************************************/
 package org.apache.cayenne.reflect.pojo;
 
+import java.util.Iterator;
+import java.util.Map;
+
 import org.apache.cayenne.Persistent;
 import org.apache.cayenne.ValueHolder;
+import org.apache.cayenne.exp.Expression;
 import org.apache.cayenne.reflect.Accessor;
 import org.apache.cayenne.reflect.ClassDescriptor;
+import org.apache.cayenne.reflect.PropertyException;
+import org.apache.cayenne.reflect.ToManyMapProperty;
 import org.apache.cayenne.util.PersistentObjectMap;
 
 /**
  * @since 3.0
  * @author Andrus Adamchik
  */
-class EnhancedPojoMapProperty extends EnhancedPojoToManyProperty {
+class EnhancedPojoMapProperty extends EnhancedPojoToManyProperty implements
+        ToManyMapProperty {
+
+    private Expression mapKey;
 
     EnhancedPojoMapProperty(ClassDescriptor owner, ClassDescriptor targetDescriptor,
             Accessor accessor, String reverseName) {
         super(owner, targetDescriptor, accessor, reverseName);
+
+        this.mapKey = getRelationship().getMapKeyExpression();
     }
 
     protected ValueHolder createValueHolder(Persistent relationshipOwner) {
-        return new PersistentObjectMap(relationshipOwner, getName(), getRelationship()
-                .getMapKeyExpression());
+        return new PersistentObjectMap(relationshipOwner, getName(), mapKey);
+    }
+
+    public void remapTarget(Object source, Object target) throws PropertyException {
+        // TODO: andrus, 9/24/2007 - this is an clone of the remapTarget method in
+        // DataObjectToManyMapProperty that is a part of a different inheritance
+        // hierarchy... need to reconcile that somehow...
+
+        if (target == null) {
+            throw new NullPointerException("Null target");
+        }
+
+        Map map = (Map) readProperty(source);
+
+        Object newKey = mapKey.evaluate(target);
+        Object currentValue = map.get(newKey);
+
+        if (currentValue == target) {
+            // nothing to do
+            return;
+        }
+        // else - do not check for conflicts here (i.e. another object mapped for the same
+        // key), as we have no control of the order in which this method is called, so
+        // another object may be remapped later by the caller
+
+        // must do a slow map scan to ensure the object is not mapped under a different
+        // key...
+        Iterator it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry e = (Map.Entry) it.next();
+            if (e.getValue() == target) {
+                it.remove();
+                break;
+            }
+        }
+
+        map.put(newKey, target);
     }
 }
