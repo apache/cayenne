85848eb8f5dcf938208ac5509af37751500a9d74
Andrus Adamchik <aadamchik@apache.org>
2007-10-09 21:19:25 +0000

    CAY-875 DbAdapter for SQLite (initial adapter. Unit tests stats stand at Failures: 16, Errors: 29)
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@583302 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/cayenne-jdk1.4-unpublished/src/main/java/org/apache/cayenne/dba/sqlite/SQLiteBigDecimalType.java
@@ -0,0 +1,53 @@
+/*****************************************************************
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ ****************************************************************/
+package org.apache.cayenne.dba.sqlite;
+
+import java.math.BigDecimal;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+
+import org.apache.cayenne.access.types.AbstractType;
+
+/**
+ * @since 3.0
+ * @author Andrus Adamchik
+ */
+class SQLiteBigDecimalType extends AbstractType {
+
+    public String getClassName() {
+        return BigDecimal.class.getName();
+    }
+
+    public Object materializeObject(CallableStatement rs, int index, int type)
+            throws Exception {
+        // BigDecimals are not supported by the zentus driver... in addition the driver
+        // throws an NPE on 'getDouble' if the value is null, so must read it as an
+        // object.
+        Number n = (Number) rs.getObject(index);
+        return (n == null) ? null : new BigDecimal(n.doubleValue());
+    }
+
+    public Object materializeObject(ResultSet rs, int index, int type) throws Exception {
+        // BigDecimals are not supported by the zentus driver... in addition the driver
+        // throws an NPE on 'getDouble' if the value is null, so must read it as an
+        // object.
+        Number n = (Number) rs.getObject(index);
+        return (n == null) ? null : new BigDecimal(n.doubleValue());
+    }
+}
