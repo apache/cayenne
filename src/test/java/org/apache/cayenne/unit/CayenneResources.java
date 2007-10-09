032d018e74f51ab2e6b5806dfa84d67e7252d387
Andrus Adamchik <aadamchik@apache.org>
2007-10-09 19:21:30 +0000

    undoing a recent change that allowed non-pooled DS - this addition is not needed and the condition it was supposed to solve is bogus
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk/framework/cayenne-jdk1.4-unpublished@583264 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/cayenne/unit/CayenneResources.java
+++ src/test/java/org/apache/cayenne/unit/CayenneResources.java
@@ -31,7 +31,6 @@ import org.apache.cayenne.CayenneRuntimeException;
 import org.apache.cayenne.access.DataNode;
 import org.apache.cayenne.conf.ConnectionProperties;
 import org.apache.cayenne.conn.DataSourceInfo;
-import org.apache.cayenne.conn.DriverDataSource;
 import org.apache.cayenne.conn.PoolDataSource;
 import org.apache.cayenne.conn.PoolManager;
 import org.apache.cayenne.unit.util.SQLTemplateCustomizer;
@@ -298,27 +297,15 @@ public class CayenneResources implements BeanFactoryAware {
     public DataSource createDataSource() {
 
         try {
-            AccessStackAdapter adapter = getAccessStackAdapter(Class
-                    .forName(connectionInfo.getAdapterClassName()));
-
-            if (adapter.usePooledDataSource()) {
-                PoolDataSource poolDS = new PoolDataSource(
-                        connectionInfo.getJdbcDriver(),
-                        connectionInfo.getDataSourceUrl());
-                return new PoolManager(
-                        poolDS,
-                        1,
-                        1,
-                        connectionInfo.getUserName(),
-                        connectionInfo.getPassword());
-            }
-            else {
-                return new DriverDataSource(
-                        connectionInfo.getJdbcDriver(),
-                        connectionInfo.getDataSourceUrl(),
-                        connectionInfo.getUserName(),
-                        connectionInfo.getPassword());
-            }
+            PoolDataSource poolDS = new PoolDataSource(
+                    connectionInfo.getJdbcDriver(),
+                    connectionInfo.getDataSourceUrl());
+            return new PoolManager(
+                    poolDS,
+                    1,
+                    1,
+                    connectionInfo.getUserName(),
+                    connectionInfo.getPassword());
         }
         catch (Exception ex) {
             logObj.error("Can not create shared data source.", ex);
