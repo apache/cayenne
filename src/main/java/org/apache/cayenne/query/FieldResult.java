7996a14d45b823d449ae97edfbd93f265875babc
Andrus Adamchik <aadamchik@apache.org>
2008-02-09 14:00:47 +0000

    CAY-839 Support combination of Persistent objects and scalars in query results
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk/framework/cayenne-jdk1.5-unpublished@620111 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/cayenne/query/FieldResult.java
@@ -0,0 +1,52 @@
+/*****************************************************************
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ ****************************************************************/
+package org.apache.cayenne.query;
+
+/**
+ * A metadata object that maps a result set column to an ObjAttribute or DbAttribute. Used
+ * by {@link EntityResult}.
+ * 
+ * @since 3.0
+ * @author Andrus Adamchik
+ */
+public class FieldResult {
+
+    protected String attributeName;
+    protected String column;
+    protected boolean dbAttribute;
+
+    public FieldResult(String attributeName, String column, boolean dbAttribute) {
+        this.attributeName = attributeName;
+        this.column = column;
+        this.dbAttribute = dbAttribute;
+    }
+
+    public String getAttributeName() {
+        return attributeName;
+    }
+
+    public String getColumn() {
+        return column;
+    }
+
+    public boolean isDbAttribute() {
+        return dbAttribute;
+    }
+
+}
