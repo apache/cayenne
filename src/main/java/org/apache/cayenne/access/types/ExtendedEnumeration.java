e5a3d388e074e0bfe02a4e4f95eb5cc3485f59b7
Michael Ray Gentry <mgentry@apache.org>
2008-03-02 16:05:54 +0000

    Changed comment.
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk/framework/cayenne-jdk1.5-unpublished@632777 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/cayenne/access/types/ExtendedEnumeration.java
+++ src/main/java/org/apache/cayenne/access/types/ExtendedEnumeration.java
@@ -2,7 +2,9 @@ package org.apache.cayenne.access.types;
 
 public interface ExtendedEnumeration
 {
-    // Return the value to be stored in the database for this enumeration.  In
-    // actuallity, this should be an Integer or a String.
+    /**
+     * Return the value to be stored in the database for this enumeration.  In
+     * actuality, this should be an Integer or a String.
+     */
     public Object getDatabaseValue();
 }
