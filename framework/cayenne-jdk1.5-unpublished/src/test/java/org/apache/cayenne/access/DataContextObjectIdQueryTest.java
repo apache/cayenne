05b8e901ed8ba12735a08ba493d5d41e8ff7924c
Andrus Adamchik <aadamchik@apache.org>
2008-04-07 11:10:39 +0000

    a test case trying to reproduce a bug with ObjectIdQuery cache_refresh policy.. the bug is NOT confirmed.
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@645460 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/cayenne-jdk1.5-unpublished/src/test/java/org/apache/cayenne/access/DataContextObjectIdQueryTest.java
@@ -0,0 +1,63 @@
+/*****************************************************************
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ ****************************************************************/
+package org.apache.cayenne.access;
+
+import java.util.Date;
+
+import org.apache.art.Artist;
+import org.apache.cayenne.DataObjectUtils;
+import org.apache.cayenne.ObjectId;
+import org.apache.cayenne.query.ObjectIdQuery;
+import org.apache.cayenne.query.SQLTemplate;
+import org.apache.cayenne.unit.CayenneCase;
+
+public class DataContextObjectIdQueryTest extends CayenneCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        deleteTestData();
+    }
+
+    public void testRefreshNullifiedValues() {
+
+        DataContext context = createDataContext();
+
+        Artist a = context.newObject(Artist.class);
+        a.setArtistName("X");
+        a.setDateOfBirth(new Date());
+
+        context.commitChanges();
+
+        context.performGenericQuery(new SQLTemplate(
+                Artist.class,
+                "UPDATE ARTIST SET DATE_OF_BIRTH = NULL"));
+
+        long id = DataObjectUtils.longPKForObject(a);
+        ObjectIdQuery query = new ObjectIdQuery(new ObjectId(
+                "Artist",
+                Artist.ARTIST_ID_PK_COLUMN,
+                id), false, ObjectIdQuery.CACHE_REFRESH);
+
+        Artist a1 = (Artist) DataObjectUtils.objectForQuery(context, query);
+        assertNull(a1.getDateOfBirth());
+        assertEquals("X", a1.getArtistName());
+    }
+}
