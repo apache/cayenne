baa891d6c3524c1fd947da2e3ac0629ceab8e5d4
Andrus Adamchik <aadamchik@apache.org>
2008-04-22 07:35:19 +0000

    vararg
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@650401 13f79535-47bb-0310-9956-ffa450edef68

--- framework/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/util/BrowserControl.java
+++ framework/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/util/BrowserControl.java
@@ -55,7 +55,7 @@ public class BrowserControl {
             }
             else if (OperatingSystem.getOS() == OperatingSystem.MAC_OS_X) {
                 Class<?> fileManager = Class.forName("com.apple.eio.FileManager");
-                Method openURL = fileManager.getDeclaredMethod("openURL", new Class[] {String.class});
+                Method openURL = fileManager.getDeclaredMethod("openURL", String.class);
                 openURL.invoke(null, new Object[] {url});
             }
             else { // assume Unix or Linux
