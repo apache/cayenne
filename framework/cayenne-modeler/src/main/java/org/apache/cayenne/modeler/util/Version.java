9207fa0a893734c79b94958eb51870b5b32c120e
Michael Ray Gentry <mgentry@apache.org>
2008-05-15 15:16:04 +0000

    Added generics to Comparable interface to remove warnings.
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@656696 13f79535-47bb-0310-9956-ffa450edef68

--- framework/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/util/Version.java
+++ framework/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/util/Version.java
@@ -26,7 +26,7 @@ import java.util.StringTokenizer;
  * 
  * @author Andrus Adamchik
  */
-public class Version implements Comparable {
+public class Version implements Comparable<Object> {
 
     protected String versionString;
     protected int[] versionParts;
