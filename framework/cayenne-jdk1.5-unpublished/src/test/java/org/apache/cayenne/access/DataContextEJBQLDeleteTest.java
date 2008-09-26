018a44af5acec1bf378205fb208b935ac0d37d75
Andrus Adamchik <aadamchik@apache.org>
2008-09-26 11:36:15 +0000

    patch testing EJBQL delete with an IN
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@699283 13f79535-47bb-0310-9956-ffa450edef68

--- framework/cayenne-jdk1.5-unpublished/src/test/java/org/apache/cayenne/access/DataContextEJBQLDeleteTest.java
+++ framework/cayenne-jdk1.5-unpublished/src/test/java/org/apache/cayenne/access/DataContextEJBQLDeleteTest.java
@@ -18,6 +18,9 @@
  ****************************************************************/
 package org.apache.cayenne.access;
 
+import java.util.List;
+
+import org.apache.art.MeaningfulPKTest1;
 import org.apache.art.Painting;
 import org.apache.cayenne.DataObjectUtils;
 import org.apache.cayenne.QueryResponse;
@@ -26,9 +29,12 @@ import org.apache.cayenne.unit.CayenneCase;
 
 public class DataContextEJBQLDeleteTest extends CayenneCase {
     
+    protected DataContext context;
+    
     @Override
     protected void setUp() throws Exception {
         deleteTestData();
+        context = getDomain().createDataContext();
     }
     
     public void testDeleteNoIdVar() throws Exception {
@@ -76,5 +82,36 @@ public class DataContextEJBQLDeleteTest extends CayenneCase {
                 .objectForPK(createDataContext(), Painting.class, 33001));
         assertNull(DataObjectUtils
                 .objectForPK(createDataContext(), Painting.class, 33002));
+    }
+    
+    public void testDeleteIdVar() throws Exception {
+        insertValue();
+        
+        EJBQLQuery q = new EJBQLQuery("select m.pkAttribute from MeaningfulPKTest1 m");
+    
+        List<Integer> id = createDataContext().performQuery(q);
+       
+        String ejbql = "delete from MeaningfulPKTest1 m WHERE m.pkAttribute in (:id)";
+         
+        EJBQLQuery query = new EJBQLQuery(ejbql);
+        query.setParameter("id", id);
+        QueryResponse result = createDataContext().performGenericQuery(query);
+    
+        int[] count = result.firstUpdateCount();
+        assertNotNull(count);
+        assertEquals(1, count.length);
+        assertEquals(420, count[0]);
+
+    }
+    
+    public void insertValue(){
+        MeaningfulPKTest1 obj ;
+        
+        for(int i=0;i<420;i++){
+            obj = (MeaningfulPKTest1) context.newObject("MeaningfulPKTest1");
+            obj.setPkAttribute(new Integer(i));
+            obj.setDescr("a" + i);
+            context.commitChanges();
+        }
     }
 }
