4480520c510eb096bdcb87ca40ad3a0378d11d34
Andrus Adamchik <aadamchik@apache.org>
2009-05-17 20:19:10 +0000

    fixing project property setup procedure for the latest maven
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@775750 13f79535-47bb-0310-9956-ffa450edef68

--- build-tools/maven-cayenne-build-plugin/src/main/java/org/apache/cayenne/maven/plugin/date/DateMojo.java
+++ build-tools/maven-cayenne-build-plugin/src/main/java/org/apache/cayenne/maven/plugin/date/DateMojo.java
@@ -26,13 +26,14 @@ import java.util.Date;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
 
 /**
  * Exports the "project.build.date" and "project.build.time" properties to the
  * environment.
  * 
  * @goal date
- * @phase generate-resources
+ * @phase initialize
  * @requiresProject
  */
 public class DateMojo extends AbstractMojo {
@@ -44,6 +45,15 @@ public class DateMojo extends AbstractMojo {
     static final String BUILD_DATE_FORMAT = "MMM dd yyyy";
     static final String BUILD_TIME_FORMAT = "HH:mm:ss";
 
+    /**
+     * POM
+     * 
+     * @parameter expression="${project}"
+     * @readonly
+     * @required
+     */
+    protected MavenProject project;
+
     public void execute() throws MojoExecutionException, MojoFailureException {
         Format dateFormat = new SimpleDateFormat(BUILD_DATE_FORMAT);
         Format timeFormat = new SimpleDateFormat(BUILD_TIME_FORMAT);
@@ -54,8 +64,10 @@ public class DateMojo extends AbstractMojo {
         calendar.add(Calendar.MILLISECOND, -offset);
         Date gmtTime = calendar.getTime();
 
-        System.setProperty(BUILD_DATE_PROPERTY, dateFormat.format(gmtTime));
-        System.setProperty(BUILD_TIME_PROPERTY, timeFormat.format(gmtTime));
-        System.setProperty(BUILD_YEAR_PROPERTY, String.valueOf(calendar.get(Calendar.YEAR)));
+        project.getProperties().put(BUILD_DATE_PROPERTY, dateFormat.format(gmtTime));
+        project.getProperties().put(BUILD_TIME_PROPERTY, timeFormat.format(gmtTime));
+        project.getProperties().put(
+                BUILD_YEAR_PROPERTY,
+                String.valueOf(calendar.get(Calendar.YEAR)));
     }
 }
