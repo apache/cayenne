0461b111fc5d97eee40700af3b05038bd5f8ee51
Andrey Razumovsky <andrey@apache.org>
2009-10-05 18:58:00 +0000

    CAY-1286 Update Cayenne Modeler validation errors panel to resize correctly
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@821962 13f79535-47bb-0310-9956-ffa450edef68

--- framework/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/dialog/validator/ValidatorDialog.java
+++ framework/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/dialog/validator/ValidatorDialog.java
@@ -119,7 +119,7 @@ public class ValidatorDialog extends CayenneDialog {
         CellConstraints cc = new CellConstraints();
         PanelBuilder builder = new PanelBuilder(new FormLayout(
                 "fill:200dlu:grow",
-                "pref, 3dlu, top:40dlu:grow"));
+                "pref, 3dlu, fill:40dlu:grow"));
 
         builder.setDefaultDialogBorder();
 
