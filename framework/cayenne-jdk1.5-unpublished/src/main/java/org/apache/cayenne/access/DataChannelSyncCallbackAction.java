4208a7cd8396a26bedcc72cf8a8ea7005a29d6bf
Andrus Adamchik <aadamchik@apache.org>
2010-11-10 20:12:53 +0000

    CAY-1505 Callbacks: POST_UPDATE is called on updated removed object, instead of POST_REMOVE
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/branches/STABLE-3.0@1033690 13f79535-47bb-0310-9956-ffa450edef68

--- framework/cayenne-jdk1.5-unpublished/src/main/java/org/apache/cayenne/access/DataChannelSyncCallbackAction.java
+++ framework/cayenne-jdk1.5-unpublished/src/main/java/org/apache/cayenne/access/DataChannelSyncCallbackAction.java
@@ -20,8 +20,8 @@ package org.apache.cayenne.access;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.cayenne.DataChannel;
 import org.apache.cayenne.graph.GraphChangeHandler;
@@ -34,6 +34,10 @@ import org.apache.cayenne.reflect.LifecycleCallbackRegistry;
  * @since 3.0
  */
 abstract class DataChannelSyncCallbackAction implements GraphChangeHandler {
+
+    static enum Op {
+        INSERT, UPDATE, DELETE
+    }
 
     static DataChannelSyncCallbackAction getCallbackAction(
             LifecycleCallbackRegistry callbackRegistry,
@@ -56,7 +60,7 @@ abstract class DataChannelSyncCallbackAction implements GraphChangeHandler {
     Collection updated;
     Collection persisted;
     Collection removed;
-    private Set<Object> seenIds;
+    private Map<Object, Op> seenIds;
     private GraphManager graphManager;
 
     DataChannelSyncCallbackAction(LifecycleCallbackRegistry callbackRegistry,
@@ -66,7 +70,7 @@ abstract class DataChannelSyncCallbackAction implements GraphChangeHandler {
         this.graphManager = graphManager;
 
         if (hasListeners()) {
-            this.seenIds = new HashSet<Object>();
+            this.seenIds = new HashMap<Object, Op>();
             changes.apply(this);
         }
     }
@@ -84,13 +88,14 @@ abstract class DataChannelSyncCallbackAction implements GraphChangeHandler {
     }
 
     public void nodeCreated(Object nodeId) {
-        if (seenIds.add(nodeId)) {
+        Op op = seenIds.put(nodeId, Op.INSERT);
+        if (op == null) {
 
             Object node = graphManager.getNode(nodeId);
             if (node != null) {
 
                 if (persisted == null) {
-                    persisted = new ArrayList();
+                    persisted = new ArrayList<Object>();
                 }
 
                 persisted.add(node);
@@ -99,16 +104,26 @@ abstract class DataChannelSyncCallbackAction implements GraphChangeHandler {
     }
 
     public void nodeRemoved(Object nodeId) {
-        if (seenIds.add(nodeId)) {
+        Op op = seenIds.put(nodeId, Op.DELETE);
+
+        // the node may have been updated prior to delete
+        if (op != Op.DELETE) {
 
             Object node = graphManager.getNode(nodeId);
             if (node != null) {
 
                 if (removed == null) {
-                    removed = new ArrayList();
+                    removed = new ArrayList<Object>();
                 }
 
                 removed.add(node);
+
+                if (op == Op.UPDATE) {
+                    updated.remove(node);
+                }
+
+                // don't care about preceding Op.INSERT, as NEW -> DELETED objects are
+                // purged from the change log upstream and we don't see them here
             }
         }
     }
@@ -135,13 +150,16 @@ abstract class DataChannelSyncCallbackAction implements GraphChangeHandler {
     }
 
     private void nodeUpdated(Object nodeId) {
-        if (seenIds.add(nodeId)) {
+        
+        Op op = seenIds.put(nodeId, Op.UPDATE);
+        
+        if (op == null) {
 
             Object node = graphManager.getNode(nodeId);
             if (node != null) {
 
                 if (updated == null) {
-                    updated = new ArrayList();
+                    updated = new ArrayList<Object>();
                 }
 
                 updated.add(node);
