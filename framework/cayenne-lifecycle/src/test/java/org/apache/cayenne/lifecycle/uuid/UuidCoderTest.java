08229c1957eeb8c4e57bde93a43491028c96966d
Andrus Adamchik <aadamchik@apache.org>
2011-01-28 14:16:22 +0000

    CAY-1532 Import cayenne-lifecycle from sandbox
    package name fix in tests
    
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@1064687 13f79535-47bb-0310-9956-ffa450edef68

--- framework/cayenne-lifecycle/src/test/java/org/apache/cayenne/lifecycle/uuid/UuidCoderTest.java
+++ framework/cayenne-lifecycle/src/test/java/org/apache/cayenne/lifecycle/uuid/UuidCoderTest.java
@@ -16,7 +16,7 @@
  *  specific language governing permissions and limitations
  *  under the License.
  ****************************************************************/
-package org.apache.cayenne.mixin.uuid;
+package org.apache.cayenne.lifecycle.uuid;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
