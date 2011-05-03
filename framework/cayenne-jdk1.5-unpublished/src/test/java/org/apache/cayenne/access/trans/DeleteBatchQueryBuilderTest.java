9e070a5702a3287e9d0efd6cad2ca5f77ebfb97f
Andrus Adamchik <aadamchik@apache.org>
2011-05-03 13:59:36 +0000

    CAY-1563 CayenneCase unit tests conversion(3)
    patches by Dzmitry Kazimirchyk:
    DeleteBatchQueryBuilderTest
    
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@1099057 13f79535-47bb-0310-9956-ffa450edef68

--- framework/cayenne-jdk1.5-unpublished/src/test/java/org/apache/cayenne/access/trans/DeleteBatchQueryBuilderTest.java
+++ framework/cayenne-jdk1.5-unpublished/src/test/java/org/apache/cayenne/access/trans/DeleteBatchQueryBuilderTest.java
@@ -24,16 +24,25 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import org.apache.cayenne.configuration.server.ServerRuntime;
 import org.apache.cayenne.dba.DbAdapter;
 import org.apache.cayenne.dba.JdbcAdapter;
+import org.apache.cayenne.di.Inject;
+import org.apache.cayenne.map.DbAttribute;
 import org.apache.cayenne.map.DbEntity;
 import org.apache.cayenne.query.DeleteBatchQuery;
 import org.apache.cayenne.testdo.locking.SimpleLockingTestEntity;
-import org.apache.cayenne.unit.LockingCase;
+import org.apache.cayenne.unit.di.server.ServerCase;
+import org.apache.cayenne.unit.di.server.UseServerRuntime;
 
-/**
- */
-public class DeleteBatchQueryBuilderTest extends LockingCase {
+@UseServerRuntime(ServerCase.LOCKING_PROJECT)
+public class DeleteBatchQueryBuilderTest extends ServerCase {
+
+    @Inject
+    private ServerRuntime runtime;
+
+    @Inject
+    private DbAdapter adapter;
 
     public void testConstructor() throws Exception {
         DbAdapter adapter = new JdbcAdapter();
@@ -44,10 +53,10 @@ public class DeleteBatchQueryBuilderTest extends LockingCase {
     }
 
     public void testCreateSqlString() throws Exception {
-        DbEntity entity = getDomain().getEntityResolver().lookupObjEntity(
+        DbEntity entity = runtime.getDataDomain().getEntityResolver().lookupObjEntity(
                 SimpleLockingTestEntity.class).getDbEntity();
 
-        List idAttributes = Collections.singletonList(entity
+        List<DbAttribute> idAttributes = Collections.singletonList((DbAttribute) entity
                 .getAttribute("LOCKING_TEST_ID"));
 
         DeleteBatchQuery deleteQuery = new DeleteBatchQuery(entity, idAttributes, null, 1);
@@ -60,13 +69,14 @@ public class DeleteBatchQueryBuilderTest extends LockingCase {
     }
 
     public void testCreateSqlStringWithNulls() throws Exception {
-        DbEntity entity = getDomain().getEntityResolver().lookupObjEntity(
+        DbEntity entity = runtime.getDataDomain().getEntityResolver().lookupObjEntity(
                 SimpleLockingTestEntity.class).getDbEntity();
 
-        List idAttributes = Arrays.asList(entity.getAttribute("LOCKING_TEST_ID"), entity
+        List<DbAttribute> idAttributes = Arrays.asList((DbAttribute) entity
+                .getAttribute("LOCKING_TEST_ID"), (DbAttribute) entity
                 .getAttribute("NAME"));
 
-        Collection nullAttributes = Collections.singleton("NAME");
+        Collection<String> nullAttributes = Collections.singleton("NAME");
 
         DeleteBatchQuery deleteQuery = new DeleteBatchQuery(
                 entity,
@@ -82,23 +92,23 @@ public class DeleteBatchQueryBuilderTest extends LockingCase {
     }
 
     public void testCreateSqlStringWithIdentifiersQuote() throws Exception {
-        DbEntity entity = getDomain().getEntityResolver().lookupObjEntity(
+        DbEntity entity = runtime.getDataDomain().getEntityResolver().lookupObjEntity(
                 SimpleLockingTestEntity.class).getDbEntity();
         try {
 
             entity.getDataMap().setQuotingSQLIdentifiers(true);
-            List idAttributes = Collections.singletonList(entity
-                    .getAttribute("LOCKING_TEST_ID"));
+            List<DbAttribute> idAttributes = Collections
+                    .singletonList((DbAttribute) entity.getAttribute("LOCKING_TEST_ID"));
 
             DeleteBatchQuery deleteQuery = new DeleteBatchQuery(
                     entity,
                     idAttributes,
                     null,
                     1);
-            JdbcAdapter adapter = (JdbcAdapter) getAccessStackAdapter().getAdapter();
+            JdbcAdapter adapter = (JdbcAdapter) this.adapter;
             DeleteBatchQueryBuilder builder = new DeleteBatchQueryBuilder(adapter);
             String generatedSql = builder.createSqlString(deleteQuery);
-            
+
             String charStart = adapter.getIdentifiersStartQuote();
             String charEnd = adapter.getIdentifiersEndQuote();
 
@@ -120,26 +130,26 @@ public class DeleteBatchQueryBuilderTest extends LockingCase {
     }
 
     public void testCreateSqlStringWithNullsWithIdentifiersQuote() throws Exception {
-        DbEntity entity = getDomain().getEntityResolver().lookupObjEntity(
+        DbEntity entity = runtime.getDataDomain().getEntityResolver().lookupObjEntity(
                 SimpleLockingTestEntity.class).getDbEntity();
         try {
 
             entity.getDataMap().setQuotingSQLIdentifiers(true);
 
-            List idAttributes = Arrays.asList(
-                    entity.getAttribute("LOCKING_TEST_ID"),
-                    entity.getAttribute("NAME"));
+            List<DbAttribute> idAttributes = Arrays.asList((DbAttribute) entity
+                    .getAttribute("LOCKING_TEST_ID"), (DbAttribute) entity
+                    .getAttribute("NAME"));
 
-            Collection nullAttributes = Collections.singleton("NAME");
+            Collection<String> nullAttributes = Collections.singleton("NAME");
 
             DeleteBatchQuery deleteQuery = new DeleteBatchQuery(
                     entity,
                     idAttributes,
                     nullAttributes,
                     1);
-            
-            JdbcAdapter adapter = (JdbcAdapter) getAccessStackAdapter().getAdapter();
-            
+
+            JdbcAdapter adapter = (JdbcAdapter) this.adapter;
+
             DeleteBatchQueryBuilder builder = new DeleteBatchQueryBuilder(adapter);
             String generatedSql = builder.createSqlString(deleteQuery);
 
