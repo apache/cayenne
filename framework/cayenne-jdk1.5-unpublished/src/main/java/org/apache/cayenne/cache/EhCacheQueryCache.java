dbbb3eb5eee54d7639afb66e894a5712ce694607
Dzmitry Kazimirchyk <dkazimirchyk@apache.org>
2011-07-28 14:55:26 +0000

    CAY-1595 EHCache implementation of Provider<QueryCache>
    basic implementation of EhCacheQueryCache + some unit tests modification for it
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@1151882 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/cayenne-jdk1.5-unpublished/src/main/java/org/apache/cayenne/cache/EhCacheQueryCache.java
@@ -0,0 +1,187 @@
+/*****************************************************************
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ ****************************************************************/
+package org.apache.cayenne.cache;
+
+import java.util.List;
+
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.Element;
+
+import org.apache.cayenne.CayenneRuntimeException;
+import org.apache.cayenne.di.BeforeScopeEnd;
+import org.apache.cayenne.query.QueryMetadata;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+public class EhCacheQueryCache implements QueryCache {
+    
+    /**
+     * Default cache group name.
+     */
+    static final String DEFAULT_CACHE_NAME = "cayenne.default.cachegroup";
+    
+    private static Log logger = LogFactory.getLog(EhCacheQueryCache.class);
+    
+    protected CacheManager cacheManager;
+    
+    public EhCacheQueryCache() {
+        cacheManager = new CacheManager();
+        init();
+    }
+    
+    public EhCacheQueryCache(String configFile) {
+        cacheManager = new CacheManager(configFile);
+        init();
+    }
+    
+    public EhCacheQueryCache(CacheManager cacheManager) {
+        if (cacheManager != null) {
+            this.cacheManager = cacheManager;
+            init();
+        }
+        else {
+            throw new CayenneRuntimeException("CacheManager cannot be null.");
+        }
+    }
+    
+    private void init() {
+        cacheManager.addCacheIfAbsent(DEFAULT_CACHE_NAME);
+    }
+
+    public List get(QueryMetadata metadata) {
+        String key = metadata.getCacheKey();
+        if (key == null) {
+            return null;
+        }
+        
+        Element result = null;
+        String[] groupNames = metadata.getCacheGroups();
+        if (groupNames != null && groupNames.length > 0) {
+            Ehcache cache = cacheManager.getCache(groupNames[0]);
+            if (cache == null) {
+                return null;
+            }
+            else {
+                result = cache.get(key);
+                if (result != null) {
+                    return (List)result.getObjectValue();
+                }
+            }
+            if (groupNames.length > 1) {
+                logger.warn("multiple cache groups per key: " + key);
+            }
+        }
+        else {
+            result = getDefaultCache().get(key);
+            if (result != null) {
+                return (List)getDefaultCache().get(key).getObjectValue();
+            }
+        }
+        return null;
+    }
+
+    public List get(QueryMetadata metadata, QueryCacheEntryFactory factory) {
+        String key = metadata.getCacheKey();
+        if (key == null) {
+            return null;
+        }
+        
+        List result = get(metadata);
+        if (result != null) {
+            return result;
+        }
+        else {
+            Object noResult = factory.createObject();
+            if (!(noResult instanceof List)) {
+                if (noResult == null) {
+                    throw new CayenneRuntimeException("Null object created: "
+                            + metadata.getCacheKey());
+                }
+                else {
+                    throw new CayenneRuntimeException(
+                            "Invalid query result, expected List, got "
+                            + noResult.getClass().getName());
+                }
+            }
+
+            List list = (List)noResult;
+            put(metadata, list);
+            return list;
+        }
+    }
+
+    public void put(QueryMetadata metadata, List results) {
+        String key = metadata.getCacheKey();
+        if (key != null) {
+            String[] groupNames = metadata.getCacheGroups();
+            if (groupNames != null && groupNames.length > 0) {
+                Ehcache cache = cacheManager.addCacheIfAbsent(groupNames[0]);
+                cache.put(new Element(key, results));
+                if (groupNames.length > 1) {
+                    logger.warn("multiple groups per key: " + key);
+                }
+            }
+            else {
+                getDefaultCache().put(new Element(key,results));
+            }
+        }
+    }
+
+    public void remove(String key) {
+        if (key != null) {
+            for (String cache : cacheManager.getCacheNames()) {
+                cacheManager.getCache(cache).remove(key);
+            }
+        }
+        
+    }
+
+    public void removeGroup(String groupKey) {
+        cacheManager.removeCache(groupKey);
+    }
+
+    public void clear() {
+        cacheManager.removalAll();
+    }
+
+    public int size() {
+        int size = 0;
+        for (String cache : cacheManager.getCacheNames()) {
+            size += cacheManager.getCache(cache).getSize();
+        }
+        return size;
+    }
+
+    /**
+     * Returns default cache group.
+     */
+    public Ehcache getDefaultCache() {
+        return cacheManager.getCache(DEFAULT_CACHE_NAME);
+    }
+    
+    /**
+     * Shuts down EhCache CacheManager
+     */
+    @BeforeScopeEnd
+    public void shutdown() {
+        cacheManager.shutdown();
+    }
+}
---
+++ framework/cayenne-jdk1.5-unpublished/src/test/java/org/apache/cayenne/access/DataContextQueryCachingEhCacheTest.java
@@ -0,0 +1,61 @@
+/*****************************************************************
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ ****************************************************************/
+package org.apache.cayenne.access;
+
+import org.apache.cayenne.cache.EhCacheQueryCache;
+import org.apache.cayenne.test.jdbc.TableHelper;
+import org.apache.cayenne.unit.di.server.ServerCase;
+import org.apache.cayenne.unit.di.server.UseServerRuntime;
+
+@UseServerRuntime(ServerCase.TESTMAP_PROJECT)
+public class DataContextQueryCachingEhCacheTest extends DataContextQueryCachingTest {
+    
+    protected EhCacheQueryCache domainCache;
+    protected EhCacheQueryCache contextCache;
+
+    @Override
+    protected void setUpAfterInjection() throws Exception {
+        dbHelper.deleteAll("PAINTING");
+        dbHelper.deleteAll("ARTIST");
+
+        tArtist = new TableHelper(dbHelper, "ARTIST");
+        tArtist.setColumns("ARTIST_ID", "ARTIST_NAME");
+
+        tPainting = new TableHelper(dbHelper, "PAINTING");
+        tPainting.setColumns(
+                "PAINTING_ID",
+                "PAINTING_TITLE",
+                "ARTIST_ID",
+                "ESTIMATED_PRICE");
+
+        domain = context.getParentDataDomain();
+        oldCache = domain.getQueryCache();
+        
+        domainCache = new EhCacheQueryCache();
+        contextCache = new EhCacheQueryCache();
+        domain.setQueryCache(domainCache);
+        context.setQueryCache(contextCache);
+    }
+    
+    @Override
+    protected void tearDownBeforeInjection() throws Exception {
+        domainCache.shutdown();
+        contextCache.shutdown();
+    }
+}
