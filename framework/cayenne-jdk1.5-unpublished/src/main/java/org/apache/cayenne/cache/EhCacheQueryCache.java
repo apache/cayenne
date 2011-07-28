52c69f457dfc04eef4b3ff4f70461e1828a2333c
Dzmitry Kazimirchyk <dkazimirchyk@apache.org>
2011-07-28 16:43:32 +0000

    CAY-1595 EHCache implementation of Provider<QueryCache>
    correcting some multithreading problems
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@1151922 13f79535-47bb-0310-9956-ffa450edef68

--- framework/cayenne-jdk1.5-unpublished/src/main/java/org/apache/cayenne/cache/EhCacheQueryCache.java
+++ framework/cayenne-jdk1.5-unpublished/src/main/java/org/apache/cayenne/cache/EhCacheQueryCache.java
@@ -104,27 +104,66 @@ public class EhCacheQueryCache implements QueryCache {
             return null;
         }
         
-        List result = get(metadata);
-        if (result != null) {
-            return result;
+        Ehcache cache = null;
+        Element result = null;
+        String[] groupNames = metadata.getCacheGroups();
+        if (groupNames != null && groupNames.length > 0) {
+            cache = cacheManager.getCache(groupNames[0]);
+            if (cache == null) {
+                return null;
+            }
+            else {
+                result = cache.get(key);
+            }
+            if (groupNames.length > 1) {
+                logger.warn("multiple cache groups per key: " + key);
+            }
         }
         else {
-            Object noResult = factory.createObject();
-            if (!(noResult instanceof List)) {
-                if (noResult == null) {
-                    throw new CayenneRuntimeException("Null object created: "
-                            + metadata.getCacheKey());
-                }
-                else {
-                    throw new CayenneRuntimeException(
-                            "Invalid query result, expected List, got "
-                            + noResult.getClass().getName());
-                }
-            }
+            cache = getDefaultCache();
+            result = cache.get(key);
+        }
 
-            List list = (List)noResult;
-            put(metadata, list);
-            return list;
+        if (result != null) {
+            return (List)result.getObjectValue();
+        }
+
+        // if no result in cache locking the key to write
+        // and putting it to the cache
+        cache.acquireWriteLockOnKey(key);
+        try {
+            
+            // trying to read from cache again in case of
+            // someone else put it to the cache before us
+            List list = get(metadata);
+            
+            if (list == null) {
+                
+                // if not succeeded  in reading again putting
+                // object to the cache ourselves
+                Object noResult = factory.createObject();
+                if (!(noResult instanceof List)) {
+                    if (noResult == null) {
+                        throw new CayenneRuntimeException("Null object created: "
+                                + metadata.getCacheKey());
+                    }
+                    else {
+                        throw new CayenneRuntimeException(
+                                "Invalid query result, expected List, got "
+                                + noResult.getClass().getName());
+                    }
+                }
+
+                list = (List)noResult;
+                put(metadata, list);
+                return list;
+            }
+            else {
+                return list;
+            }
+        }
+        finally {
+            cache.releaseWriteLockOnKey(key);
         }
     }
 
