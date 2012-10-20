3be07dbfa2cd64de74a21b27158eaacfe5268ba8
Andrus Adamchik <aadamchik@apache.org>
2012-10-20 22:39:43 +0000

    allowing for slower hardware to finish the deadlock test
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@1400527 13f79535-47bb-0310-9956-ffa450edef68

--- framework/cayenne-jdk1.5-unpublished/src/test/java/org/apache/cayenne/access/NestedDataContext_DeadlockTest.java
+++ framework/cayenne-jdk1.5-unpublished/src/test/java/org/apache/cayenne/access/NestedDataContext_DeadlockTest.java
@@ -24,7 +24,7 @@ import java.util.Random;
 import org.apache.cayenne.ObjectContext;
 import org.apache.cayenne.configuration.server.ServerRuntime;
 import org.apache.cayenne.di.Inject;
-import org.apache.cayenne.query.EJBQLQuery;
+import org.apache.cayenne.query.SelectQuery;
 import org.apache.cayenne.test.jdbc.DBHelper;
 import org.apache.cayenne.test.jdbc.TableHelper;
 import org.apache.cayenne.test.parallel.ParallelTestContainer;
@@ -35,94 +35,94 @@ import org.apache.cayenne.unit.di.server.UseServerRuntime;
 @UseServerRuntime(ServerCase.TESTMAP_PROJECT)
 public class NestedDataContext_DeadlockTest extends ServerCase {
 
-	@Inject
-	private DataContext parent;
+    @Inject
+    private DataContext parent;
 
-	@Inject
-	private ServerRuntime runtime;
+    @Inject
+    private ServerRuntime runtime;
 
-	@Inject
-	protected DBHelper dbHelper;
+    @Inject
+    protected DBHelper dbHelper;
 
-	protected TableHelper tArtist;
+    protected TableHelper tArtist;
 
-	@Override
-	protected void setUpAfterInjection() throws Exception {
-		dbHelper.deleteAll("PAINTING_INFO");
-		dbHelper.deleteAll("PAINTING");
-		dbHelper.deleteAll("ARTIST_EXHIBIT");
-		dbHelper.deleteAll("ARTIST_GROUP");
-		dbHelper.deleteAll("ARTIST");
+    @Override
+    protected void setUpAfterInjection() throws Exception {
+        dbHelper.deleteAll("PAINTING_INFO");
+        dbHelper.deleteAll("PAINTING");
+        dbHelper.deleteAll("ARTIST_EXHIBIT");
+        dbHelper.deleteAll("ARTIST_GROUP");
+        dbHelper.deleteAll("ARTIST");
 
-		tArtist = new TableHelper(dbHelper, "ARTIST");
-		tArtist.setColumns("ARTIST_ID", "ARTIST_NAME");
-	}
+        tArtist = new TableHelper(dbHelper, "ARTIST");
+        tArtist.setColumns("ARTIST_ID", "ARTIST_NAME");
+    }
 
-	private void createArtists() throws Exception {
-		for (int i = 0; i < 300; i++) {
-			tArtist.insert(i + 1, "X" + i);
-		}
-	}
+    private void createArtists() throws Exception {
+        for (int i = 0; i < 300; i++) {
+            tArtist.insert(i + 1, "X" + i);
+        }
+    }
 
-	public void testDeadlock() throws Exception {
+    public void testDeadlock() throws Exception {
 
-		createArtists();
+        createArtists();
 
-		final Thread[] threads = new Thread[2];
+        final Thread[] threads = new Thread[2];
 
-		Random rnd = new Random(System.currentTimeMillis());
-		for (int i = 0; i < threads.length; i++) {
-			threads[i] = new UpdateThread("UpdateThread-" + i,
-					runtime.getContext(parent), rnd);
-		}
+        Random rnd = new Random(System.currentTimeMillis());
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new UpdateThread("UpdateThread-" + i,
+                    runtime.getContext(parent), rnd);
+        }
 
-		for (int i = 0; i < threads.length; i++) {
-			threads[i].start();
-		}
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].start();
+        }
 
-		new ParallelTestContainer() {
+        new ParallelTestContainer() {
 
-			@Override
-			protected void assertResult() throws Exception {
-				for (int i = 0; i < threads.length; i++) {
-					// unfortunately here we'll have to leave some dead threads
-					// behind... Of course if there's no deadlock, there won't
-					// be a leak either
-					assertTrue("Deadlocked thread", !threads[i].isAlive());
-				}
-			}
-		}.runTest(2000);
+            @Override
+            protected void assertResult() throws Exception {
+                for (int i = 0; i < threads.length; i++) {
+                    // unfortunately here we'll have to leave some dead threads
+                    // behind... Of course if there's no deadlock, there won't
+                    // be a leak either
+                    assertFalse("Deadlocked thread", threads[i].isAlive());
+                }
+            }
+        }.runTest(10000);
 
-	}
+    }
 
-	static class UpdateThread extends Thread {
+    static class UpdateThread extends Thread {
 
-		protected ObjectContext nestedContext;
-		protected Random rnd;
+        protected ObjectContext nestedContext;
+        protected Random rnd;
 
-		UpdateThread(String name, ObjectContext nestedContext, Random rnd) {
-			super(name);
-			setDaemon(true);
-			this.nestedContext = nestedContext;
-			this.rnd = rnd;
-		}
+        UpdateThread(String name, ObjectContext nestedContext, Random rnd) {
+            super(name);
+            setDaemon(true);
+            this.nestedContext = nestedContext;
+            this.rnd = rnd;
+        }
 
-		@Override
-		public void run() {
+        @Override
+        public void run() {
 
-			List<Artist> artists = nestedContext.performQuery(new EJBQLQuery(
-					"select a FROM Artist a"));
+            List<Artist> artists = nestedContext.performQuery(new SelectQuery(
+                    Artist.class));
 
-			for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < 100; i++) {
 
-				for (int j = 0; j < 5; j++) {
-					int index = rnd.nextInt(artists.size());
-					Artist a = artists.get(index);
-					a.setArtistName("Y" + rnd.nextInt());
-				}
+                for (int j = 0; j < 5; j++) {
+                    int index = rnd.nextInt(artists.size());
+                    Artist a = artists.get(index);
+                    a.setArtistName("Y" + rnd.nextInt());
+                }
 
-				nestedContext.commitChanges();
-			}
-		}
-	}
+                nestedContext.commitChanges();
+            }
+        }
+    }
 }
