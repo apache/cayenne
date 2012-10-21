29639f9d8bbc48d8c7838beb8d8588f765a141e3
Andrus Adamchik <aadamchik@apache.org>
2012-10-21 18:42:33 +0000

    unit test data cleanup
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/branches/STABLE-3.1@1400693 13f79535-47bb-0310-9956-ffa450edef68

--- framework/cayenne-jdk1.5-unpublished/src/test/java/org/apache/cayenne/access/DataContextQueryCachingOSCacheTest.java
+++ framework/cayenne-jdk1.5-unpublished/src/test/java/org/apache/cayenne/access/DataContextQueryCachingOSCacheTest.java
@@ -29,7 +29,10 @@ public class DataContextQueryCachingOSCacheTest extends DataContextQueryCachingT
     // runs super tests with a different setup...
     @Override
     protected void setUpAfterInjection() throws Exception {
+        dbHelper.deleteAll("PAINTING_INFO");
         dbHelper.deleteAll("PAINTING");
+        dbHelper.deleteAll("ARTIST_EXHIBIT");
+        dbHelper.deleteAll("ARTIST_GROUP");
         dbHelper.deleteAll("ARTIST");
 
         tArtist = new TableHelper(dbHelper, "ARTIST");
--- framework/cayenne-jdk1.5-unpublished/src/test/java/org/apache/cayenne/access/DataContextQueryCachingTest.java
+++ framework/cayenne-jdk1.5-unpublished/src/test/java/org/apache/cayenne/access/DataContextQueryCachingTest.java
@@ -57,8 +57,11 @@ public class DataContextQueryCachingTest extends ServerCase {
 
     @Override
     protected void setUpAfterInjection() throws Exception {
-        dbHelper.deleteAll("ARTIST");
+        dbHelper.deleteAll("PAINTING_INFO");
         dbHelper.deleteAll("PAINTING");
+        dbHelper.deleteAll("ARTIST_EXHIBIT");
+        dbHelper.deleteAll("ARTIST_GROUP");
+        dbHelper.deleteAll("ARTIST");
 
         tArtist = new TableHelper(dbHelper, "ARTIST");
         tArtist.setColumns("ARTIST_ID", "ARTIST_NAME");
