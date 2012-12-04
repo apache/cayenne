91a7bb4a6dbd0e5885e83636281a1ab97d07bb62
Andrus Adamchik <aadamchik@apache.org>
2012-12-04 17:51:57 +0000

    CAY-1708  Modeler error when pasting datamap with EJBQL querry
    unused import
    
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@1417064 13f79535-47bb-0310-9956-ffa450edef68

--- modeler/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/action/PasteAction.java
+++ modeler/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/action/PasteAction.java
@@ -28,7 +28,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
 
 import org.apache.cayenne.configuration.ConfigurationNode;
