98d2e3b0e5c878920b2975fe39ccc8feeaf711f6
Andrus Adamchik <aadamchik@apache.org>
2012-12-06 12:05:52 +0000

    CAY-1778 TransactionManager to simplify user-managed transactions
    (shortening ServerRuntime tx api)
    
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@1417811 13f79535-47bb-0310-9956-ffa450edef68

--- framework/cayenne-jdk1.5-unpublished/src/main/java/org/apache/cayenne/configuration/server/ServerRuntime.java
+++ framework/cayenne-jdk1.5-unpublished/src/main/java/org/apache/cayenne/configuration/server/ServerRuntime.java
@@ -28,13 +28,15 @@ import org.apache.cayenne.configuration.CayenneRuntime;
 import org.apache.cayenne.configuration.rop.client.ClientRuntime;
 import org.apache.cayenne.di.Module;
 import org.apache.cayenne.tx.TransactionManager;
+import org.apache.cayenne.tx.TransactionalOperation;
 
 /**
- * An object representing Cayenne server-stack that connects directly to the database via
- * JDBC. This is an entry point for user applications to access Cayenne, which
- * encapsulates the dependency injection internals. The term "server" is used as opposed
- * to ROP "client" (see {@link ClientRuntime}). Any application, desktop, server, etc.
- * that has a direct JDBC connection should be using this runtime.
+ * An object representing Cayenne server-stack that connects directly to the
+ * database via JDBC. This is an entry point for user applications to access
+ * Cayenne, which encapsulates the dependency injection internals. The term
+ * "server" is used as opposed to ROP "client" (see {@link ClientRuntime}). Any
+ * application, desktop, server, etc. that has a direct JDBC connection should
+ * be using this runtime.
  * 
  * @since 3.1
  */
@@ -45,49 +47,58 @@ public class ServerRuntime extends CayenneRuntime {
     }
 
     /**
-     * Creates a server runtime configuring it with a standard set of services contained
-     * in {@link ServerModule}. CayenneServerModule is created with provided
-     * 'configurationLocation'. An optional array of extra modules may contain service
-     * overrides and/or user services.
+     * Creates a server runtime configuring it with a standard set of services
+     * contained in {@link ServerModule}. CayenneServerModule is created with
+     * provided 'configurationLocation'. An optional array of extra modules may
+     * contain service overrides and/or user services.
      */
     public ServerRuntime(String configurationLocation, Module... extraModules) {
         super(mergeModules(mainModule(configurationLocation), extraModules));
     }
 
     /**
-     * Creates a server runtime configuring it with a standard set of services contained
-     * in {@link ServerModule}. CayenneServerModule is created with one or more
-     * 'configurationLocations'. An optional array of extra modules may contain service
-     * overrides and/or user services.
+     * Creates a server runtime configuring it with a standard set of services
+     * contained in {@link ServerModule}. CayenneServerModule is created with
+     * one or more 'configurationLocations'. An optional array of extra modules
+     * may contain service overrides and/or user services.
      */
     public ServerRuntime(String[] configurationLocations, Module... extraModules) {
         super(mergeModules(mainModule(configurationLocations), extraModules));
     }
-    
+
     /**
+     * Runs provided operation wrapped in a single transaction. Transaction
+     * handling delegated to the internal {@link TransactionManager}. Nested
+     * calls to 'performInTransaction' are safe and attached to the same
+     * in-progress transaction. TransactionalOperation can be some arbitrary
+     * user code, which most often than not will consist of multiple Cayenne
+     * operations.
+     * 
      * @since 3.2
      */
-    public TransactionManager getTransactionManager() {
-        return injector.getInstance(TransactionManager.class);
+    public <T> T performInTransaction(TransactionalOperation<T> op) {
+        TransactionManager tm = injector.getInstance(TransactionManager.class);
+        return tm.performInTransaction(op);
     }
 
     /**
-     * Returns the main runtime DataDomain. Note that by default the returned DataDomain
-     * is the same as the main DataChannel returned by {@link #getChannel()}. Although
-     * users may redefine DataChannel provider in the DI registry, for instance to
-     * decorate this DataDomain with a custom wrapper.
+     * Returns the main runtime DataDomain. Note that by default the returned
+     * DataDomain is the same as the main DataChannel returned by
+     * {@link #getChannel()}. Although users may redefine DataChannel provider
+     * in the DI registry, for instance to decorate this DataDomain with a
+     * custom wrapper.
      */
     public DataDomain getDataDomain() {
         return injector.getInstance(DataDomain.class);
     }
 
     /**
-     * Provides access to the JDBC DataSource assigned to a given DataNode. A null
-     * argument will work if there's only one DataNode configured.
+     * Provides access to the JDBC DataSource assigned to a given DataNode. A
+     * null argument will work if there's only one DataNode configured.
      * <p>
-     * Normally Cayenne applications don't need to access DataSource or any other JDBC
-     * code directly, however in some unusual conditions it may be needed, and this method
-     * provides a shortcut to raw JDBC.
+     * Normally Cayenne applications don't need to access DataSource or any
+     * other JDBC code directly, however in some unusual conditions it may be
+     * needed, and this method provides a shortcut to raw JDBC.
      */
     public DataSource getDataSource(String dataNodeName) {
         DataDomain domain = getDataDomain();
@@ -95,8 +106,7 @@ public class ServerRuntime extends CayenneRuntime {
         if (dataNodeName != null) {
             DataNode node = domain.getDataNode(dataNodeName);
             if (node == null) {
-                throw new IllegalArgumentException("Unknown DataNode name: "
-                        + dataNodeName);
+                throw new IllegalArgumentException("Unknown DataNode name: " + dataNodeName);
             }
 
             return node.getDataSource();
