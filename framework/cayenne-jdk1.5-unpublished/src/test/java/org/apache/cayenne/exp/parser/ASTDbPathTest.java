7acb40fa629e0e839abc67212e5924105564d38c
Andrus Adamchik <aadamchik@apache.org>
2013-03-15 18:28:40 +0000

    CAY-1803  Define toString() in path expressions
    preliminary unit tests
    
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@1457048 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/cayenne-jdk1.5-unpublished/src/test/java/org/apache/cayenne/exp/parser/ASTDbPathTest.java
@@ -0,0 +1,40 @@
+/*****************************************************************
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ ****************************************************************/
+package org.apache.cayenne.exp.parser;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintWriter;
+
+import junit.framework.TestCase;
+
+public class ASTDbPathTest extends TestCase {
+
+    public void testToString() {
+        assertEquals("db:x.y", new ASTDbPath("x.y").toString());
+    }
+    
+    public void testEncodeAsString() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintWriter pw = new PrintWriter(out);
+        new ASTDbPath("x.y").encodeAsString(pw);
+        pw.close();
+        
+        assertEquals("db:x.y", new String(out.toByteArray()));
+    }
+}
