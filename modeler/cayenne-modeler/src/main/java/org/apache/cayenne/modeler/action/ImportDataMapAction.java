be2551db3d5f61920ae1b0e56ce437e1c1c7c36c
Dzmitry Kazimirchyk <dkazimirchyk@apache.org>
2013-11-03 21:12:29 +0000

    CAY-1797 NPE importing DataMap
    patch by Oleg Artyukevich
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@1538454 13f79535-47bb-0310-9956-ffa450edef68

--- modeler/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/action/ImportDataMapAction.java
+++ modeler/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/action/ImportDataMapAction.java
@@ -36,6 +36,7 @@ import org.apache.cayenne.modeler.Application;
 import org.apache.cayenne.modeler.pref.FSPath;
 import org.apache.cayenne.modeler.util.CayenneAction;
 import org.apache.cayenne.modeler.util.FileFilters;
+import org.apache.cayenne.resource.Resource;
 import org.apache.cayenne.util.NamedObjectFactory;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -101,6 +102,13 @@ public class ImportDataMapAction extends CayenneAction {
             }
             else {
                 newMap.setName(NamedObjectFactory.createName(DataMap.class, domain));
+            }
+            
+            Resource baseResource = domain.getConfigurationSource();
+
+            if(baseResource != null) {
+                Resource dataMapResource = baseResource.getRelativeResource(newMap.getName());
+                newMap.setConfigurationSource(dataMapResource);
             }
 
             getProjectController().addDataMap(this, newMap);
