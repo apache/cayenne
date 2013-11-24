a86a2b6720edc72b06901f235901758253a2a059
Andrus Adamchik <aadamchik@apache.org>
2013-11-24 08:49:48 +0000

    CAY-1882 Porting to OSGi environment
    a basic OSGi Module
    * AdhocObjectFactory with Osgi-friendly classloaders
    
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@1544944 13f79535-47bb-0310-9956-ffa450edef68

---
+++ cayenne-server/src/main/java/org/apache/cayenne/configuration/osgi/SplitClassLoaderAdhocObjectFactory.java
@@ -0,0 +1,72 @@
+/*****************************************************************
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ ****************************************************************/
+package org.apache.cayenne.configuration.osgi;
+
+import org.apache.cayenne.di.AdhocObjectFactory;
+import org.apache.cayenne.di.spi.DefaultAdhocObjectFactory;
+
+/**
+ * An OSGi-friendly {@link AdhocObjectFactory} that tries to detect the right
+ * ClassLoader for any given resource based on the package name.
+ * 
+ * @since 3.2
+ */
+public class SplitClassLoaderAdhocObjectFactory extends DefaultAdhocObjectFactory {
+
+    private static final String CAYENNE_PACKAGE = "org/apache/cayenne";
+    private static final String CAYENNE_DI_PACKAGE_SUFFIX = "/di";
+
+    private ClassLoader cayenneServerClassLoader;
+    private ClassLoader cayenneDiClassLoader;
+
+    public SplitClassLoaderAdhocObjectFactory() {
+        this.cayenneDiClassLoader = AdhocObjectFactory.class.getClassLoader();
+        this.cayenneServerClassLoader = SplitClassLoaderAdhocObjectFactory.class.getClassLoader();
+    }
+
+    @Override
+    public ClassLoader getClassLoader(String resourceName) {
+
+        if (resourceName == null || resourceName.length() < 2) {
+            return applicationClassLoader(resourceName);
+        }
+
+        String normalizedName = resourceName.charAt(0) == '/' ? resourceName.substring(1) : resourceName;
+        if (normalizedName.startsWith(CAYENNE_PACKAGE)) {
+
+            return (normalizedName.substring(CAYENNE_PACKAGE.length()).startsWith(CAYENNE_DI_PACKAGE_SUFFIX)) ? cayenneDiClassLoader()
+                    : cayenneServerClassLoader();
+        }
+
+        return applicationClassLoader(resourceName);
+    }
+
+    protected ClassLoader applicationClassLoader(String resourceName) {
+        return super.getClassLoader(resourceName);
+    }
+
+    protected ClassLoader cayenneDiClassLoader() {
+        return cayenneDiClassLoader;
+    }
+
+    protected ClassLoader cayenneServerClassLoader() {
+        return cayenneServerClassLoader;
+    }
+
+}
