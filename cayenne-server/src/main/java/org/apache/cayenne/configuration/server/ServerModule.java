cc1b65a33adcb7c0bd32b40d8b6f963e39650783
Andrus Adamchik <aadamchik@apache.org>
2013-11-24 15:18:06 +0000

    CAY-1882 Porting to OSGi environment
    making sure overdidden AOF is still injected everywhere
    
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@1544993 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne-server/src/main/java/org/apache/cayenne/configuration/server/ServerModule.java
+++ cayenne-server/src/main/java/org/apache/cayenne/configuration/server/ServerModule.java
@@ -80,6 +80,7 @@ import org.apache.cayenne.dba.sqlserver.SQLServerSniffer;
 import org.apache.cayenne.dba.sybase.SybaseSniffer;
 import org.apache.cayenne.di.AdhocObjectFactory;
 import org.apache.cayenne.di.Binder;
+import org.apache.cayenne.di.Key;
 import org.apache.cayenne.di.ListBuilder;
 import org.apache.cayenne.di.Module;
 import org.apache.cayenne.di.spi.DefaultAdhocObjectFactory;
@@ -127,29 +128,43 @@ public class ServerModule implements Module {
         binder.bindMap(Constants.PROPERTIES_MAP)
             .put(Constants.SERVER_MAX_ID_QUALIFIER_SIZE_PROPERTY, String.valueOf(DEFAULT_MAX_ID_QUALIFIER_SIZE));
 
-        binder.bind(JdbcEventLogger.class).to(CommonsJdbcEventLogger.class);
-        
-        AdhocObjectFactory objectFactory = new DefaultAdhocObjectFactory();
-        binder.bind(AdhocObjectFactory.class).toInstance(objectFactory);
+        binder.bind(JdbcEventLogger.class).to(CommonsJdbcEventLogger.class);        
+        binder.bind(AdhocObjectFactory.class).to(DefaultAdhocObjectFactory.class);
 
         // configure known DbAdapter detectors in reverse order of popularity. Users can
         // add their own to install custom adapters automatically
-        binder
-                .bindList(Constants.SERVER_ADAPTER_DETECTORS_LIST)
-                .add(new FirebirdSniffer(objectFactory))
-                .add(new OpenBaseSniffer(objectFactory))
-                .add(new FrontBaseSniffer(objectFactory))
-                .add(new IngresSniffer(objectFactory))
-                .add(new SQLiteSniffer(objectFactory))
-                .add(new DB2Sniffer(objectFactory))
-                .add(new H2Sniffer(objectFactory))
-                .add(new HSQLDBSniffer(objectFactory))
-                .add(new SybaseSniffer(objectFactory))
-                .add(new DerbySniffer(objectFactory))
-                .add(new SQLServerSniffer(objectFactory))
-                .add(new OracleSniffer(objectFactory))
-                .add(new PostgresSniffer(objectFactory))
-                .add(new MySQLSniffer(objectFactory));
+        
+        // a bit ugly - need to bind all sniffers explicitly first before placing then in a list
+        binder.bind(FirebirdSniffer.class).to(FirebirdSniffer.class);
+        binder.bind(OpenBaseSniffer.class).to(OpenBaseSniffer.class);
+        binder.bind(FrontBaseSniffer.class).to(FrontBaseSniffer.class);
+        binder.bind(IngresSniffer.class).to(IngresSniffer.class);
+        binder.bind(SQLiteSniffer.class).to(SQLiteSniffer.class);
+        binder.bind(DB2Sniffer.class).to(DB2Sniffer.class);
+        binder.bind(H2Sniffer.class).to(H2Sniffer.class);
+        binder.bind(HSQLDBSniffer.class).to(HSQLDBSniffer.class);
+        binder.bind(SybaseSniffer.class).to(SybaseSniffer.class);
+        binder.bind(DerbySniffer.class).to(DerbySniffer.class);
+        binder.bind(SQLServerSniffer.class).to(SQLServerSniffer.class);
+        binder.bind(OracleSniffer.class).to(OracleSniffer.class);
+        binder.bind(PostgresSniffer.class).to(PostgresSniffer.class);
+        binder.bind(MySQLSniffer.class).to(MySQLSniffer.class);
+        
+        binder.bindList(Constants.SERVER_ADAPTER_DETECTORS_LIST)
+                .add(FirebirdSniffer.class)
+                .add(OpenBaseSniffer.class)
+                .add(FrontBaseSniffer.class)
+                .add(IngresSniffer.class)
+                .add(SQLiteSniffer.class)
+                .add(DB2Sniffer.class)
+                .add(H2Sniffer.class)
+                .add(HSQLDBSniffer.class)
+                .add(SybaseSniffer.class)
+                .add(DerbySniffer.class)
+                .add(SQLServerSniffer.class)
+                .add(OracleSniffer.class)
+                .add(PostgresSniffer.class)
+                .add(MySQLSniffer.class);
 
         // configure an empty filter chain
         binder.bindList(Constants.SERVER_DOMAIN_FILTERS_LIST);
