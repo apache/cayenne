25afee4da7eec5c92182dd500561f2311619e87c
Andrus Adamchik <aadamchik@apache.org>
2014-03-28 06:39:44 +0000

    CAY-1923 Optimize BatchTranslator - use fixed size array of BatchParameterBinding
    javadoc
    
    git-svn-id: https://svn.apache.org/repos/asf/cayenne/main/trunk@1582625 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne-server/src/main/java/org/apache/cayenne/access/translator/batch/BatchTranslator.java
+++ cayenne-server/src/main/java/org/apache/cayenne/access/translator/batch/BatchTranslator.java
@@ -34,16 +34,17 @@ public interface BatchTranslator {
     String getSql();
 
     /**
-     * Returns the widest possible array of bindings for this query. Each
-     * binding's position corresponds to a value position in
-     * {@link BatchQueryRow}.
+     * Returns the widest possible array of bindings for this query.
      */
     BatchParameterBinding[] getBindings();
 
     /**
      * Updates internal bindings to be used with a given row, returning updated
-     * bindings array. Note that usually the returned array is the same copy on
-     * every iteration, only with changed object state.
+     * bindings array. This method guarantees that the returned array contains
+     * the same bindings in the same order as in the array returned from
+     * {@link #getBindings()} (but in a state corresponding to the 'row'
+     * parameter). Usually the returned array is actually the same object reused
+     * for every iteration, only with changed object state.
      */
     BatchParameterBinding[] updateBindings(BatchQueryRow row);
 }
