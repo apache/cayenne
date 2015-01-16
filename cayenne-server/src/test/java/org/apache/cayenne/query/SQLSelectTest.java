e2069efeb92b1684c548af50aefa508942b5d3b8
aadamchik <aadamchik@apache.org>
2015-01-16 09:19:09 +0300

    CAY-1966 SQLTemplate/SQLSelect positional parameter binding
    * positional parameters must be included in cache key

--- cayenne-server/src/test/java/org/apache/cayenne/query/SQLSelectTest.java
+++ cayenne-server/src/test/java/org/apache/cayenne/query/SQLSelectTest.java
@@ -20,6 +20,8 @@ package org.apache.cayenne.query;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertThat;
@@ -115,5 +117,22 @@ public class SQLSelectTest {
 		SQLSelect<DataRow> q = SQLSelect.dataRowQuery("bla").paramsArray("a", "b");
 		SQLTemplate replacement = (SQLTemplate) q.createReplacementQuery(mock(EntityResolver.class));
 		assertArrayEquals(new Object[] { "a", "b" }, replacement.getPositionalParams().toArray());
+	}
+
+	@Test
+	public void testGetMetadata_ParamsArray_Multiple_Cache() {
+
+		EntityResolver resolver = mock(EntityResolver.class);
+		QueryMetadata md0 = SQLSelect.dataRowQuery("bla").localCache().getMetaData(resolver);
+		QueryMetadata md1 = SQLSelect.dataRowQuery("bla").localCache().paramsArray("a").getMetaData(resolver);
+		QueryMetadata md2 = SQLSelect.dataRowQuery("bla").localCache().paramsArray("a", "b").getMetaData(resolver);
+
+		assertNotNull(md0.getCacheKey());
+		assertNotNull(md1.getCacheKey());
+		assertNotNull(md2.getCacheKey());
+		
+		assertNotEquals(md0.getCacheKey(), md1.getCacheKey());
+		assertNotEquals(md0.getCacheKey(), md2.getCacheKey());
+		assertNotEquals(md1.getCacheKey(), md2.getCacheKey());
 	}
 }
