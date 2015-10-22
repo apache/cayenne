ed288811f8fd645a3d17e5ecdebeefe843e4e8c3
aadamchik <aadamchik@apache.org>
2015-10-22 17:32:47 +0300

    CAY-2030 Capturing a stream of commit changes
    * flattened relationship test

---
+++ cayenne-lifecycle/src/test/java/org/apache/cayenne/lifecycle/db/E3.java
@@ -0,0 +1,9 @@
+package org.apache.cayenne.lifecycle.db;
+
+import org.apache.cayenne.lifecycle.db.auto._E3;
+
+public class E3 extends _E3 {
+
+    private static final long serialVersionUID = 1L; 
+
+}
---
+++ cayenne-lifecycle/src/test/java/org/apache/cayenne/lifecycle/db/E4.java
@@ -0,0 +1,9 @@
+package org.apache.cayenne.lifecycle.db;
+
+import org.apache.cayenne.lifecycle.db.auto._E4;
+
+public class E4 extends _E4 {
+
+    private static final long serialVersionUID = 1L; 
+
+}
