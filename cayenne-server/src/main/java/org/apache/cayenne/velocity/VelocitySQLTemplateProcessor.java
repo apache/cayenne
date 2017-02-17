207c086f8d859f427f82f6a2236c42942bc9fb39
Markus Reich <reich.markus@gmail.com>
2017-02-17 12:37:30 +0100

    Added missing java.util.concurrent packages
    ConcurrentHashMap,ConcurrentMap

--- cayenne-server/src/main/java/org/apache/cayenne/velocity/VelocitySQLTemplateProcessor.java
+++ cayenne-server/src/main/java/org/apache/cayenne/velocity/VelocitySQLTemplateProcessor.java
@@ -41,6 +41,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Processor for SQL velocity templates.
