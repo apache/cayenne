bc7399a313b4fa55f3f083106cbe2012c9397e97
Nikita Timofeev <stariy95@gmail.com>
2017-08-10 18:27:21 +0300

    CAY-2351 Remove commons-collections usage completely   Own ReferenceMap implementation

--- cayenne-server/src/main/java/org/apache/cayenne/util/ReferenceMap.java
+++ cayenne-server/src/main/java/org/apache/cayenne/util/ReferenceMap.java
@@ -25,33 +25,68 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.lang.ref.Reference;
 import java.lang.ref.ReferenceQueue;
+import java.util.AbstractMap;
+import java.util.AbstractSet;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 /**
  * Map that transparently stores values as references and resolves them as needed.
+ * Though this implementation try to follow general {@link Map} contract (including equals() and hashCode())
+ * it is not intended for general usage.
  * <p>
- * This implementation supports serialization.
+ * There is no HardReferenceMap as simple HashMap can be used for that.
  * <p>
- * Internally data stored in HashMap thus this class and all implementations are not thread safe.
+ * This map doesn't guaranties that value will be there even right after put(), as GC can remove it at any time.
+ * <p>
+ * This implementation supports proper serialization, concrete classes should use {@link #writeObjectInternal(ObjectOutputStream)}
+ * and {@link #readObjectInternal(ObjectInputStream)} methods to support it too.
+ * <p>
  *
- * @see WeakValueMap
- * @see SoftValueMap
+ * @param <K> key type
+ * @param <V> value type
+ * @param <R> reference type that will be used to store values
+ *
+ * @see WeakValueMap implementation that uses WeakReference to store values
+ * @see SoftValueMap implementation that uses SoftReference to store values
  *
  * @since 4.1
  */
-public abstract class ReferenceMap<K, V, R extends Reference<V>> implements Map<K, V>, Serializable {
+abstract class ReferenceMap<K, V, R extends Reference<V>> extends AbstractMap<K, V> implements Serializable {
+
+    /*
+     * Implementation notes:
+     *  - internally data stored in HashMap thus this class and all implementations are not thread safe;
+     *  - to track references that were cleared ReferenceQueue is used;
+     *  - this map stores not only direct key => ref map but also a reverse ref => key to be able
+     *  effectively clear data that was removed by GC;
+     *  - this map is abstract, all that required for the concrete implementation is
+     *  to define newReference(Object) method;
+     *  - all accessors/modifiers should call checkReferenceQueue() to clear all stale data
+     */
 
     private static final long serialVersionUID = -3365744592038165092L;
 
+    /**
+     * This is a main data storage used for most operations
+     */
     protected transient Map<K, R> map;
 
+    /**
+     * This is aux storage to faster remove cleared references
+     */
     protected transient Map<R, K> reverseMap;
 
     protected transient ReferenceQueue<V> referenceQueue;
+
+    /**
+     * This is a lazily created set of entries that is essentially a view to actual data
+     */
+    protected transient Set<Entry<K, V>> entrySet;
 
     public ReferenceMap() {
         map = new HashMap<>();
@@ -163,13 +198,12 @@ public abstract class ReferenceMap<K, V, R extends Reference<V>> implements Map<
     @Override
     public Collection<V> values() {
         checkReferenceQueue();
-        Collection<V> values = new ArrayList<>();
-        for(R v : map.values()) {
+        // this can be optimized by creating view instead of new heavyweight collection
+        Collection<R> referenceValues = map.values();
+        Collection<V> values = new ArrayList<>(referenceValues.size());
+        for(R v : referenceValues) {
             if(v != null) {
-                V value = v.get();
-                if(value != null) {
-                    values.add(value);
-                }
+                values.add(v.get());
             }
         }
         return values;
@@ -177,11 +211,17 @@ public abstract class ReferenceMap<K, V, R extends Reference<V>> implements Map<
 
     @Override
     public Set<Entry<K, V>> entrySet() {
-        throw new UnsupportedOperationException();
+        checkReferenceQueue();
+        // lazily create entry set view
+        Set<Entry<K, V>> es = entrySet;
+        if(es == null) {
+            entrySet = es = new ReferenceEntrySet();
+        }
+        return es;
     }
 
     /**
-     * Cleanup all collected references
+     * Cleanup all references collected by GC so far
      */
     protected void checkReferenceQueue() {
         Reference<? extends V> reference;
@@ -212,7 +252,6 @@ public abstract class ReferenceMap<K, V, R extends Reference<V>> implements Map<
         writeObjectInternal(out);
     }
 
-    @SuppressWarnings("unchecked")
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
         readObjectInternal(in);
     }
@@ -235,8 +274,70 @@ public abstract class ReferenceMap<K, V, R extends Reference<V>> implements Map<
         reverseMap = new HashMap<>(replacement.size());
         referenceQueue = new ReferenceQueue<>();
         putAll(replacement);
-        map.forEach((k, v) -> {
-            reverseMap.put(v, k);
-        });
+        map.forEach((k, v) -> reverseMap.put(v, k));
+    }
+
+    /**
+     * View over {@link #map} entry set
+     */
+    class ReferenceEntrySet extends AbstractSet<Entry<K, V>> {
+
+        @Override
+        public Iterator<Entry<K, V>> iterator() {
+            return new ReferenceEntryIterator();
+        }
+
+        @Override
+        public int size() {
+            return map.size();
+        }
+    }
+
+    /**
+     * Iterator used by entrySet. Wrapper around {@link #map} iterator
+     */
+    class ReferenceEntryIterator implements Iterator<Entry<K, V>> {
+
+        Iterator<Entry<K, R>> internalIterator;
+
+        ReferenceEntryIterator() {
+            internalIterator = map.entrySet().iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return internalIterator.hasNext();
+        }
+
+        @Override
+        public Entry<K, V> next() {
+            return new ReferenceEntry(internalIterator.next());
+        }
+    }
+
+    /**
+     * View over {@link Map.Entry} that transparently resolves Reference
+     */
+    class ReferenceEntry extends SimpleEntry<K, V> {
+
+        private static final long serialVersionUID = -1795136249842496011L;
+
+        Entry<K, R> refEntry;
+
+        public ReferenceEntry(Entry<K, R> refEntry) {
+            super(refEntry.getKey(), refEntry.getValue() != null ? refEntry.getValue().get() : null);
+            this.refEntry = refEntry;
+        }
+
+        @Override
+        public V setValue(V value) {
+            R newRef = newReference(value);
+            R oldRef = refEntry.setValue(newRef);
+            reverseMap.put(newRef, reverseMap.remove(oldRef));
+            if(oldRef != null) {
+                return oldRef.get();
+            }
+            return null;
+        }
     }
 }
