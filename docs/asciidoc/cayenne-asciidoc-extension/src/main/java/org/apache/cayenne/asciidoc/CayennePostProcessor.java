319e589709f5378b41d7d3a4ec683bdf0f3c8276
Nikita Timofeev <stariy95@gmail.com>
2018-04-27 17:58:10 +0300

    CAY-2408 Cayenne JDK 10 compatibility

--- docs/asciidoc/cayenne-asciidoc-extension/src/main/java/org/apache/cayenne/asciidoc/CayennePostProcessor.java
+++ docs/asciidoc/cayenne-asciidoc-extension/src/main/java/org/apache/cayenne/asciidoc/CayennePostProcessor.java
@@ -30,7 +30,6 @@ import java.util.Map;
 
 import org.asciidoctor.Options;
 import org.asciidoctor.ast.Document;
-import org.asciidoctor.ast.DocumentRuby;
 import org.asciidoctor.extension.Postprocessor;
 import org.jsoup.Jsoup;
 
@@ -59,8 +58,12 @@ public class CayennePostProcessor extends Postprocessor {
     private static final String POSITION_BODY = "body";
     private static final String POSITION_BOTTOM = "bottom";
 
-    public CayennePostProcessor(DocumentRuby documentRuby) {
+    public CayennePostProcessor() {
         super();
+    }
+
+    public CayennePostProcessor(Map<String, Object> config) {
+        super(config);
     }
 
     public String process(Document document, String output) {
