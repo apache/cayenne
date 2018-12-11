371cab36beed351bb788255a31e2dae36de40dcd
Arseni Bulatski <ancarseni@gmail.com>
2018-12-11 17:30:27 +0300

    Update cgen icon. Update dmg.

--- assembly/src/main/resources/assemblies/mac/create-dmg.sh
+++ assembly/src/main/resources/assemblies/mac/create-dmg.sh
@@ -3,10 +3,10 @@
 # Based on https://gist.github.com/asmaloney/55d96a8c3558b2f92cb3
 
 VERSION="$1"
-DMG_BACKGROUND_IMG="src/main/resources/assemblies/mac/background.png"
- 
+DMG_BACKGROUND_IMG="src/main/resources/assemblies/mac/background.tiff"
+
 APP_EXE="CayenneModeler.app/Contents/MacOS/CayenneModeler"
- 
+
 VOL_NAME="cayenne-$1-macosx"
 DMG_TMP="target/${VOL_NAME}-temp.dmg"
 DMG_FINAL="target/${VOL_NAME}.dmg"
@@ -14,18 +14,17 @@ STAGING_DIR="target/dmg-staging"
 
 # clear out any old data
 rm -rf "${STAGING_DIR}" "${DMG_TMP}" "${DMG_FINAL}"
- 
+
 # copy over the stuff we want in the final disk image to our staging dir
 mkdir -p "${STAGING_DIR}"
 cp -rpf "target/${VOL_NAME}/CayenneModeler.app" "${STAGING_DIR}"
-cp -rpf "target/${VOL_NAME}/README.txt"         "${STAGING_DIR}"
 cp -rpf "target/${VOL_NAME}/cayenne-${VERSION}" "${STAGING_DIR}"
 
 # figure out how big our DMG needs to be
 #  assumes our contents are at least 1M!
 SIZE=`du -sh "${STAGING_DIR}" | sed 's/\([0-9]*\)M\(.*\)/\1/'`
 SIZE=`echo "${SIZE} + 1.0" | bc | awk '{print int($20+0.5)}'`
- 
+
 if [ $? -ne 0 ]; then
    echo "Error: Cannot compute size of staging dir"
    exit
@@ -34,9 +33,9 @@ fi
 # create the temp DMG file
 hdiutil create -srcfolder "${STAGING_DIR}" -volname "${VOL_NAME}" -fs HFS+ \
       -fsargs "-c c=64,a=16,e=16" -format UDRW -size ${SIZE}M "${DMG_TMP}"
- 
+
 echo "Created DMG: ${DMG_TMP}"
- 
+
 # mount it and save the device
 DEVICE=$(hdiutil attach -readwrite -noverify "${DMG_TMP}" | \
          egrep '^/dev/' | sed 1q | awk '{print $1}')
@@ -47,7 +46,7 @@ sleep 2
 pushd /Volumes/"${VOL_NAME}"
 ln -s /Applications
 popd
- 
+
 # add a background image
 mkdir /Volumes/"${VOL_NAME}"/.background
 cp "${DMG_BACKGROUND_IMG}" /Volumes/"${VOL_NAME}"/.background/
@@ -61,15 +60,14 @@ echo '
            set current view of container window to icon view
            set toolbar visible of container window to false
            set statusbar visible of container window to false
-           set the bounds of container window to {100, 100, 600, 450}
+           set the bounds of container window to {250, 100, 962, 690}
            set viewOptions to the icon view options of container window
            set arrangement of viewOptions to not arranged
-           set icon size of viewOptions to 70
-           set background picture of viewOptions to file ".background:background.png"
-           set position of item "cayenne-'${VERSION}'" of container window to {50, 240}
-           set position of item "README.txt" of container window to {360, 240}
-           set position of item "CayenneModeler.app" of container window to {50, 100}
-           set position of item "Applications" of container window to {360, 100}
+           set icon size of viewOptions to 98
+           set background picture of viewOptions to file ".background:background.tiff"
+           set position of item "cayenne-'${VERSION}'" of container window to {353, 437}
+           set position of item "CayenneModeler.app" of container window to {197, 191}
+           set position of item "Applications" of container window to {519, 190}
            close
            open
            update without registering applications
