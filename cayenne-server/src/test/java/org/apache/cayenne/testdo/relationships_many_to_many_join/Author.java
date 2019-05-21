ebdbcf90616fef1dbf0d08a224722eea4e9c0d4c
Nikita Timofeev <stariy95@gmail.com>
2019-05-21 17:24:07 +0300

    CAY-2582 Double insert of manyToMany relationship mapped to Set  test case from PR #385
    (cherry picked from commit 326f1de071cc871561891557baf5879bd1ba06ce)

---
+++ cayenne-server/src/test/java/org/apache/cayenne/testdo/relationships_many_to_many_join/Author.java
@@ -0,0 +1,9 @@
+package org.apache.cayenne.testdo.relationships_many_to_many_join;
+
+import org.apache.cayenne.testdo.relationships_many_to_many_join.auto._Author;
+
+public class Author extends _Author {
+
+    private static final long serialVersionUID = 1L; 
+
+}
