f62e5cba22e2b7b7594d33cfdc4b34f4a8f5f972
Nikita Timofeev <stariy95@gmail.com>
2020-10-02 17:56:32 +0300

    Fix propagated DB-generated PK in the vertical inheritance

---
+++ cayenne-server/src/test/java/org/apache/cayenne/testdo/inheritance_vertical/IvGenKeySub.java
@@ -0,0 +1,9 @@
+package org.apache.cayenne.testdo.inheritance_vertical;
+
+import org.apache.cayenne.testdo.inheritance_vertical.auto._IvGenKeySub;
+
+public class IvGenKeySub extends _IvGenKeySub {
+
+    private static final long serialVersionUID = 1L; 
+
+}
