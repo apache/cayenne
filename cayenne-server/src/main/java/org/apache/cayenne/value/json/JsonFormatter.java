376beca6e8bb69fa41f5259d6624e94a4c55664f
Nikita Timofeev <stariy95@gmail.com>
2020-12-02 17:55:34 +0300

    Utils for the JSON values comparison

---
+++ cayenne-server/src/main/java/org/apache/cayenne/value/json/JsonFormatter.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ *    or more contributor license agreements.  See the NOTICE file
+ *    distributed with this work for additional information
+ *    regarding copyright ownership.  The ASF licenses this file
+ *    to you under the Apache License, Version 2.0 (the
+ *    "License"); you may not use this file except in compliance
+ *    with the License.  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an
+ *    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *    KIND, either express or implied.  See the License for the
+ *    specific language governing permissions and limitations
+ *    under the License.
+ */
+
+package org.apache.cayenne.value.json;
+
+/**
+ * @since 4.2
+ */
+class JsonFormatter extends AbstractJsonConsumer<String> {
+
+    private final StringBuilder builder = new StringBuilder();
+
+    JsonFormatter(String json) {
+        super(json);
+    }
+
+    @Override
+    protected void onArrayStart() {
+        builder.append('[');
+    }
+
+    @Override
+    protected void onArrayEnd() {
+        if(builder.charAt(builder.length() - 1) == ' ') {
+            builder.delete(builder.length() - 2, builder.length());
+        }
+        builder.append(']');
+    }
+
+    @Override
+    protected void onObjectStart() {
+        builder.append('{');
+    }
+
+    @Override
+    protected void onObjectEnd() {
+        if(builder.charAt(builder.length() - 1) == ' ') {
+            builder.delete(builder.length() - 2, builder.length());
+        }
+        builder.append('}');
+    }
+
+    @Override
+    protected void onArrayValue(JsonTokenizer.JsonToken token) {
+        appendToken(token);
+        builder.append(", ");
+    }
+
+    @Override
+    protected void onObjectKey(JsonTokenizer.JsonToken token) {
+        appendToken(token);
+        builder.append(": ");
+    }
+
+    @Override
+    protected void onObjectValue(JsonTokenizer.JsonToken token) {
+        appendToken(token);
+        builder.append(", ");
+    }
+
+    @Override
+    protected void onValue(JsonTokenizer.JsonToken token) {
+        appendToken(token);
+    }
+
+    @Override
+    protected String output() {
+        return builder.toString();
+    }
+
+    private void appendToken(JsonTokenizer.JsonToken token) {
+        if(token.type == JsonTokenizer.TokenType.STRING) {
+            builder.append('"');
+        }
+        builder.append(token.getData(), token.from, token.to - token.from);
+        if(token.type == JsonTokenizer.TokenType.STRING) {
+            builder.append('"');
+        }
+    }
+}
---
+++ cayenne-server/src/main/java/org/apache/cayenne/value/json/JsonUtils.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ *    or more contributor license agreements.  See the NOTICE file
+ *    distributed with this work for additional information
+ *    regarding copyright ownership.  The ASF licenses this file
+ *    to you under the Apache License, Version 2.0 (the
+ *    "License"); you may not use this file except in compliance
+ *    with the License.  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an
+ *    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *    KIND, either express or implied.  See the License for the
+ *    specific language governing permissions and limitations
+ *    under the License.
+ */
+
+package org.apache.cayenne.value.json;
+
+/**
+ * Simple utils to process JSON.
+ *
+ * @since 4.2
+ * @see org.apache.cayenne.value.Json
+ */
+public final class JsonUtils {
+
+    /**
+     * Cleanup and reformat any valid JSON string.
+     * Generally this methods just removes unnecessary whitespaces in the document.
+     *
+     * @param json valid JSON document
+     * @return normalized JSON
+     */
+    public static String normalize(String json) {
+        return new JsonFormatter(json).process();
+    }
+
+    /**
+     * <p>
+     * Method that compares two JSON documents.
+     * <br>
+     * This methods will parse documents so it will ignores object keys ordering and whitespaces.
+     * </p>
+     * <b>NOTE</b> this method doesn't parse numbers so same numbers in different format will be different.
+     *
+     * @param json1 first value
+     * @param json2 second value
+     * @return true if documents are equal
+     */
+    public static boolean compare(String json1, String json2) {
+        Object object1 = new JsonReader(json1).process();
+        Object object2 = new JsonReader(json2).process();
+        return object1.equals(object2);
+    }
+
+    private JsonUtils() {
+    }
+
+}
