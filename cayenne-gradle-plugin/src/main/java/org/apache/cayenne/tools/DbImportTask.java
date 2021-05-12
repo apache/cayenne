8f9844d18f66e0db76d2d360911b1a9228d3f58e
dmitrij <dmitriy.tarasienko@gmail.com>
2021-05-12 11:44:36 +0300

    feat: change gradle version to 7.0 (change archive file version, replace compile configuration with implementation, add necessary getters, remove @optional above primitive values, add version 7.0 to tests of compatibility)

--- cayenne-gradle-plugin/src/main/java/org/apache/cayenne/tools/DbImportTask.java
+++ cayenne-gradle-plugin/src/main/java/org/apache/cayenne/tools/DbImportTask.java
@@ -188,6 +188,10 @@ public class DbImportTask extends BaseCayenneTask {
         setAdapter(adapter);
     }
 
+    public ReverseEngineering getReverseEngineering() {
+        return reverseEngineering;
+    }
+
     @OutputFile
     @Optional
     public File getCayenneProject() {
