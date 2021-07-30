24ee431e49068b3cad9f87a4388bfcfa27ccaf68
Nikita Timofeev <stariy95@gmail.com>
2021-07-30 16:55:35 +0300

    Improve test stability

--- cayenne-server/src/test/java/org/apache/cayenne/exp/parser/ASTFunctionCallDateIT.java
+++ cayenne-server/src/test/java/org/apache/cayenne/exp/parser/ASTFunctionCallDateIT.java
@@ -58,6 +58,8 @@ public class ASTFunctionCallDateIT extends ServerCase {
     @Before
     public void createDataSet() throws Exception {
         Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.MILLISECOND, 0);
+
         int year = cal.get(Calendar.YEAR);
         int month = cal.get(Calendar.MONTH);
         int day = cal.get(Calendar.DAY_OF_MONTH);
