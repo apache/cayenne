9c9abc71185d8dc763a85734223cd27bd3601956
Aliaksei Peraverzeu <dev.mrojaczy@gmail.com>
2022-12-12 08:41:54 +0300

    CAY-2552 fix ColumnQuery bug with Flattened attributes

--- cayenne-server/src/test/java/org/apache/cayenne/testdo/inheritance_vertical/auto/_IvImpl.java
+++ cayenne-server/src/test/java/org/apache/cayenne/testdo/inheritance_vertical/auto/_IvImpl.java
@@ -8,6 +8,7 @@ import org.apache.cayenne.exp.property.EntityProperty;
 import org.apache.cayenne.exp.property.PropertyFactory;
 import org.apache.cayenne.exp.property.StringProperty;
 import org.apache.cayenne.testdo.inheritance_vertical.IvBase;
+import org.apache.cayenne.testdo.inheritance_vertical.IvImpl;
 import org.apache.cayenne.testdo.inheritance_vertical.IvOther;
 
 /**
@@ -18,7 +19,9 @@ import org.apache.cayenne.testdo.inheritance_vertical.IvOther;
  */
 public abstract class _IvImpl extends IvBase {
 
-    private static final long serialVersionUID = 1L; 
+    private static final long serialVersionUID = 1L;
+
+    public static final EntityProperty<IvImpl> SELF = PropertyFactory.createSelf(IvImpl.class);
 
     public static final String ID_PK_COLUMN = "ID";
 
