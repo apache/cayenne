10635163af73ded5b1cd1351f55a27432b971599
Ivan Nikitka <70625960+Ivan-nikitko@users.noreply.github.com>
2023-05-30 09:35:08 +0200

    manyToMany with join table objEntity testIT

---
+++ cayenne-server/src/test/java/org/apache/cayenne/testdo/many_to_many_joinTable_objEntity/Enrollments.java
@@ -0,0 +1,9 @@
+package org.apache.cayenne.testdo.many_to_many_joinTable_objEntity;
+
+import org.apache.cayenne.testdo.many_to_many_joinTable_objEntity.auto._Enrollments;
+
+public class Enrollments extends _Enrollments {
+
+    private static final long serialVersionUID = 1L;
+
+}
