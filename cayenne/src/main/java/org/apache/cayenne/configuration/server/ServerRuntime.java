2d227715d44200b8d0bdda2a88173657c792f4a4
stariy95 <stariy95@gmail.com>
2023-11-29 13:02:58 +0400

    CAY-2820 Remove `server` from the components naming  - return ServerRuntime to o.a.c.configuration.server package

--- cayenne/src/main/java/org/apache/cayenne/configuration/server/ServerRuntime.java
+++ cayenne/src/main/java/org/apache/cayenne/configuration/server/ServerRuntime.java
@@ -16,7 +16,7 @@
  *  specific language governing permissions and limitations
  *  under the License.
  ****************************************************************/
-package org.apache.cayenne.configuration.runtime;
+package org.apache.cayenne.configuration.server;
 
 import org.apache.cayenne.runtime.CayenneRuntime;
 import org.apache.cayenne.runtime.CayenneRuntimeBuilder;
---
+++ cayenne/src/main/java/org/apache/cayenne/configuration/server/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This is a deprecated package that contained runtime creation code
+ *
+ * @deprecated since 5.0
+ * @see org.apache.cayenne.configuration.runtime
+ * @see org.apache.cayenne.runtime
+ */
+@Deprecated(since = "5.0")
+package org.apache.cayenne.configuration.server;
