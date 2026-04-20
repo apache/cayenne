apache/cayenne

commit 93e9681664459b0f77c4c3fcc5a213307f51b026

Possible exceptions in the Modeler

modeler/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/util/DbAttributePathComboBoxEditor.java
@@ -19,6 +19,7 @@
 
 package org.apache.cayenne.modeler.util;
 
+import org.apache.cayenne.exp.path.CayennePath;
 import org.apache.cayenne.map.DbAttribute;
 import org.apache.cayenne.map.DbEntity;
 import org.apache.cayenne.map.DbRelationship;
@@ -81,13 +82,12 @@ public class DbAttributePathComboBoxEditor extends PathChooserComboBoxCellEditor
 
     @Override
     protected String getPathToInitializeCombo(ObjAttributeTableModel model, int row) {
-        String pathString = model.getAttribute(row).getValue().getDbAttributePath().value();
-        if (pathString == null) {
+        CayennePath path = model.getAttribute(row).getValue().getDbAttributePath();
+        if (path == null || path.isEmpty()) {
             return "";
         }
-        String[] pathStrings = pathString.split(Pattern.quote("."));
-        String lastStringInPath = pathStrings[pathStrings.length - 1];
-        return pathString.replaceAll(lastStringInPath + '$', "");
+        return path.parent()
+                .dot(path.last().value().replaceAll("\\$", "")).value();
     }
 
     @Override

modeler/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/util/PathChooserComboBoxCellEditor.java
@@ -244,10 +244,10 @@ public abstract class PathChooserComboBoxCellEditor<T extends CayenneTableModel<
                                                       boolean isSelected, boolean cellHasFocus) {
 
             JPanel panel = new JPanel(new BorderLayout());
-            JLabel label = new JLabel((String) value);
+            JLabel label = new JLabel(value.toString());
             panel.add(label);
 
-            Object currentNode = getCurrentNode((String) value);
+            Object currentNode = getCurrentNode(value.toString());
             if (treeModel.isLeaf(currentNode)) {
                 ListCellRenderer<Object> leafRenderer = CellRenderers.listRenderer();
                 return leafRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);

commit 0e1c9a6cbe75d92ea77b98bdce2b53eb08cf55ed

Fixing regression introduced in 93e96816644

modeler/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/util/DbAttributePathComboBoxEditor.java
@@ -86,8 +86,8 @@ public class DbAttributePathComboBoxEditor extends PathChooserComboBoxCellEditor
         if (path == null || path.isEmpty()) {
             return "";
         }
-        return path.parent()
-                .dot(path.last().value().replaceAll("\\$", "")).value();
+        CayennePath parent = path.parent();
+        return parent.isEmpty() ? "" : parent.value() + ".";
     }
 
     @Override